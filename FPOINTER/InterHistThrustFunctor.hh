52f3abb11dcb026a96930df88c8b78f6704aad2c
RolfAndreassen <rolfa@slac.stanford.edu>
2013-04-10 12:01:13 -0400

    Add interpolating histogram PDF.

---
+++ FPOINTER/InterHistThrustFunctor.hh
@@ -0,0 +1,22 @@
+#ifndef INTERHIST_THRUST_FUNCTOR_HH
+#define INTERHIST_THRUST_FUNCTOR_HH
+
+#include "ThrustPdfFunctor.hh" 
+#include "BinnedDataSet.hh" 
+
+class InterHistThrustFunctor : public ThrustPdfFunctor {
+public:
+  InterHistThrustFunctor (std::string n, 
+			  BinnedDataSet* x, 
+			  std::vector<Variable*> params, 
+			  std::vector<Variable*> obses); 
+  //__host__ virtual fptype normalise () const;
+
+private:
+  thrust::device_vector<fptype>* dev_base_histogram; 
+  fptype totalEvents; 
+  fptype* host_constants;
+  int numVars; 
+};
+
+#endif

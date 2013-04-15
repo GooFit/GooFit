864277e9c421116018d231d9b91c592310bc1adf
RolfAndreassen <rolfa@slac.stanford.edu>
2013-04-15 15:26:23 -0400

    Removed non-doc files from doc branch.

--- FPOINTER/MixingTimeResolution_Aux.cu
+++
@@ -1,9 +0,0 @@
-#include "MixingTimeResolution_Aux.hh"
-#include "ThrustPdfFunctor.hh" 
-
-MixingTimeResolution::MixingTimeResolution () {}
-MixingTimeResolution::~MixingTimeResolution () {}
-
-void MixingTimeResolution::initIndex (void* dev_fcn_ptr) {
-  resFunctionIdx = ThrustPdfFunctor::findFunctionIdx(dev_fcn_ptr); 
-}

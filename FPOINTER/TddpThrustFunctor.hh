0cce522addfb4875fca00d7b193a8ec9d46897ee
RolfAndreassen <rolfa@slac.stanford.edu>
2013-07-02 09:41:25 -0400

    Move some helper stuff to separate file in preparation for time-independent Dalitz-plot class.

--- FPOINTER/TddpThrustFunctor.hh
+++ FPOINTER/TddpThrustFunctor.hh
@@ -3,87 +3,13 @@
 
 #include "ThrustPdfFunctor.hh" 
 #include "MixingTimeResolution_Aux.hh" 
-
-enum ResaonceType {FLAVOUR = 0, CP_EVEN, CP_ODD};
-enum EvaluateType {
-  RBW = 0, 
-  NONRES, 
-  GOU_SAK, 
-  GAUSSIAN // Notice that Gaussian uses mass, not mass-squared!
-};
-enum DaughterPair {PAIR_12 = 0, PAIR_13, PAIR_23}; 
-
-struct ResonanceInfo {
-  Variable* amp_real;
-  Variable* amp_imag;
-  Variable* mass;
-  Variable* width;
-  unsigned int spin;
-  unsigned int cyclic_index;
-  unsigned int eval_type;
-  unsigned int resonance_type; 
-};
-
-struct DecayInfo {
-  fptype motherMass;
-  fptype daug1Mass;
-  fptype daug2Mass;
-  fptype daug3Mass;
-  fptype meson_radius;
-
-  Variable* _tau; 
-  Variable* _xmixing;
-  Variable* _ymixing;
-  std::vector<ResonanceInfo*> resonances; 
-};
+#include "DalitzPlotHelpers.hh" 
 
 typedef thrust::tuple<fptype, fptype, fptype, fptype> WaveHolder; 
 typedef thrust::tuple<fptype, fptype, fptype, fptype, fptype, fptype> ThreeComplex;
 
 class SpecialDalitzIntegrator;
 class SpecialWaveCalculator; 
-
-// Copied from strided_range thrust example by Nathan Bell.
-// Iterator to move forward by a specified number of steps 
-// in each iteration.
-template <typename Iterator> class strided_range { 
-public:
-  typedef typename thrust::iterator_difference<Iterator>::type difference_type;
-
-  struct stride_functor : public thrust::unary_function<difference_type,difference_type> {
-    difference_type stride;
-    
-    stride_functor(difference_type stride)
-      : stride(stride) {}
-    
-    __host__ __device__ difference_type operator() (const difference_type& i) const {
-      return stride * i;
-    }
-  };
-  typedef typename thrust::counting_iterator<difference_type>                   CountingIterator;
-  typedef typename thrust::transform_iterator<stride_functor, CountingIterator> TransformIterator;
-  typedef typename thrust::permutation_iterator<Iterator,TransformIterator>     PermutationIterator;
-
-  // type of the strided_range iterator
-  typedef PermutationIterator iterator;
-
-  // construct strided_range for the range [first,last)
-  strided_range (Iterator first, Iterator last, difference_type stride)
-    : first(first), last(last), stride(stride) {}
-   
-  iterator begin () const {
-    return PermutationIterator(first, TransformIterator(CountingIterator(0), stride_functor(stride)));
-  }
-
-  iterator end () const {
-    return begin() + ((last - first) + (stride - 1)) / stride;
-  }
-  
-protected:
-  Iterator first;
-  Iterator last;
-  difference_type stride;
-};
   
 class TddpThrustFunctor : public ThrustPdfFunctor {
 public:

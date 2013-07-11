99836f78c9bc6f3331b71a48493265ac08b5cd4e
RolfAndreassen <rolfa@slac.stanford.edu>
2013-07-11 09:25:14 -0400

    Create Indexable superclass of Variable, introduce Constant.

--- Variable.cc
+++ Variable.cc
@@ -1,62 +1,52 @@
 #include "Variable.hh"
 #include <cmath> 
 
-//std::map<std::string, Variable*> Variable::nameToVariableMap;
-
 Variable::Variable (std::string n) 
-  : name(n) 
+  : Indexable(n) 
   , numbins(100)
-  , index(-1) 
   , fixed(false)
   , blind(0)
 {
 } 
 
 Variable::Variable (std::string n, fptype v) 
-  : name(n)
-  , value(v)
+  : Indexable(n, v)
   , error(0.002) 
   , lowerlimit(v - 0.01)
   , upperlimit(v + 0.01)
   , numbins(100)
-  , index(-1)
   , fixed(true)
   , blind(0)
 {
 }
 
 Variable::Variable (std::string n, fptype dn, fptype up) 
-  : name(n)
+  : Indexable(n)
   , upperlimit(up)
   , lowerlimit(dn)
   , numbins(100)
-  , index(-1)
   , fixed(false)
   , blind(0)
 {
 }
 
 Variable::Variable (std::string n, fptype v, fptype dn, fptype up) 
-  : name(n)
-  , value(v) 
+  : Indexable(n, v)
   , error(0.1*(up-dn))
   , upperlimit(up)
   , lowerlimit(dn)
   , numbins(100)
-  , index(-1)
   , fixed(false)
   , blind(0)
 {
 }
 
 Variable::Variable (std::string n, fptype v, fptype e, fptype dn, fptype up) 
-  : name(n)
-  , value(v) 
+  : Indexable(n, v)
   , error(e)
   , upperlimit(up)
   , lowerlimit(dn)
   , numbins(100)
-  , index(-1)
   , fixed(false)
   , blind(0)
 {

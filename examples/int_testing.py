2b12f62ad49fa0b316760b2f3dbe2abc02dfc6ac
Henry Schreiner <schreinf@uc.edu>
2016-12-19 12:39:09 -0500

    Include output timing

--- examples/int_testing.py
+++ examples/int_testing.py
@@ -5,6 +5,16 @@
 
 from __future__ import print_function
 from plumbum import local, cli, TEE, colors
+import time
+
+# Simple timer
+class Timer:
+    def __enter__(self):
+        self.start = time.time()
+        return self
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.interval = self.end - self.start
 
 LOCAL_DIR = local.path(__file__).dirname
 
@@ -12,24 +22,31 @@ def test(filename):
     command = local[LOCAL_DIR / filename / filename]
     colors.info.print('Running', command)
     with local.cwd(LOCAL_DIR / filename):
-        code, stdout, stderr = command & TEE(retcode=None)
+        with Timer() as t:
+            code, stdout, stderr = command & TEE(retcode=None)
         if code==0:
             colors.success.print(filename, 'Successful')
         else:
             colors.fatal.print(filename, 'Failed with status code:', code)
-    return code
+    return dict(name=filename, code=code, time=t.interval, stdout=stdout, stderr=stderr)
 
 
 class TestAll(cli.Application):
 
     def main(self):
         filelist = [f.name for f in LOCAL_DIR // '*' if f.is_dir()]
-        codes = [test(fname) for fname in filelist]
-        failed_names = [name for code, name in zip(codes, filelist) if code !=0]
-        if failed_names:
-            colors.fatal.print("Failed:", failed_names)
+        results = [test(fname) for fname in filelist]
+        failed = [result for result in results if result['code'] !=0]
+        if failed:
+            colors.fatal.print("Failed:")
+            for result in failed:
+                colors.fatal.print(result['name'], result['code'])
         else:
             colors.success.print("All programs completed.")
+        print()
+        colors.info.print('Resulting times:')
+        for result in results:
+            print((colors.success if result['code'] == 0 else colors.warn) | '{0[name]}: {0[time]}'.format(result))
 
 if __name__ == '__main__':
     TestAll()

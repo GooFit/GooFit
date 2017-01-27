95fb7db9f6de6c3562663ab1ed0c1a9efb26f8e5
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-01-27 10:57:59 -0500

    Adding better download of thrust

---
+++ cmake/AddThrust.cmake
@@ -0,0 +1,18 @@
+# 
+#
+# Downloads GTest and provides a helper macro to add tests. Add make check, as well, which
+# gives output on failed tests without having to set an environment variable.
+#
+#
+
+
+include(DownloadProject)
+download_project(PROJ                thrust
+		         GIT_REPOSITORY      https://github.com/thrust/thrust.git
+		         GIT_TAG             1.8.2      
+                 UPDATE_DISCONNECTED 1
+                 QUIET
+)
+
+set(THRUST_INCLUDE_DIRS "${thrust_SOURCE_DIR}")
+

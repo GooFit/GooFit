103c61f2394c85083fb137cc8b7aab9e0b812403
Henry Schreiner <hschrein@cern.ch>
2017-02-05 10:18:30 -0500

    Cleaning up Convolution and Product examples

--- examples/convolution/convolution.cu
+++ examples/convolution/convolution.cu
@@ -5,7 +5,6 @@
 #include "goofit/PDFs/BWPdf.h"
 #include "goofit/PDFs/GaussianPdf.h"
 #include "goofit/PDFs/ConvolutionPdf.h"
-#include <iostream>
 
 #include "TRandom.h"
 
@@ -28,32 +27,32 @@ int main(int argc, char** argv) {
     }
 
     // Independent variable.
-    Variable* xvar = new Variable("xvar", -10, 10);
+    Variable xvar{"xvar", -10, 10};
 
-    Variable* gamma = new Variable("gamma", 2, 0.1, 0.1, 5);
-    Variable* sigma = new Variable("sigma", 1.5, 0.1, 0.1, 5);
-    Variable* x0    = new Variable("x0", 0.2, 0.05, -1, 1);
-    Variable* zero  = new Variable("zero", 0);
+    Variable gamma{"gamma", 2, 0.1, 0.1, 5};
+    Variable sigma{"sigma", 1.5, 0.1, 0.1, 5};
+    Variable x0{"x0", 0.2, 0.05, -1, 1};
+    Variable zero{"zero", 0};
 
     TRandom donram(42);
     // Data set
-    UnbinnedDataSet data(xvar);
+    UnbinnedDataSet data(&xvar);
 
     // Generate toy events.
     for(int i = 0; i < 100000; ++i) {
-        xvar->value = donram.Uniform(20) - 10;
+        xvar.value = donram.Uniform(20) - 10;
 
-        double bwvalue = cpu_bw(xvar->value, x0->value, gamma->value);
-        double roll = donram.Uniform() * (2.0 / (sqrt(M_PI)*gamma->value));
+        double bwvalue = cpu_bw(xvar.value, x0.value, gamma.value);
+        double roll = donram.Uniform() * (2.0 / (sqrt(M_PI)*gamma.value));
 
         if(roll > bwvalue) {
             --i;
             continue;
         }
 
-        xvar->value += donram.Gaus(0, sigma->value);
+        xvar.value += donram.Gaus(0, sigma.value);
 
-        if((xvar->value < xvar->lowerlimit) || (xvar->value > xvar->upperlimit)) {
+        if((xvar.value < xvar.lowerlimit) || (xvar.value > xvar.upperlimit)) {
             --i;
             continue;
         }
@@ -61,12 +60,12 @@ int main(int argc, char** argv) {
         data.addEvent();
     }
 
-    BWPdf* breit = new BWPdf("breit", xvar, x0, gamma);
-    GaussianPdf* gauss = new GaussianPdf("gauss", xvar, zero, sigma);
-    ConvolutionPdf* convolution = new ConvolutionPdf("convolution", xvar, breit, gauss);
-    convolution->setData(&data);
+    BWPdf breit{"breit", &xvar, &x0, &gamma};
+    GaussianPdf gauss{"gauss", &xvar, &zero, &sigma};
+    ConvolutionPdf convolution{"convolution", &xvar, &breit, &gauss};
+    convolution.setData(&data);
 
-    FitManager fitter(convolution);
+    FitManager fitter(&convolution);
     fitter.fit();
 
     return 0;

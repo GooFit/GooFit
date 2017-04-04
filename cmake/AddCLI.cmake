80a859fd65d8e56c65248b035c3a0b110fdb984f
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-04-04 12:06:43 -0400

    Adding to new non-Makefile system

---
+++ cmake/AddCLI.cmake
@@ -0,0 +1,12 @@
+
+include(DownloadProject)
+message(STATUS "Downloading CLI if needed")
+download_project(PROJ                cli
+                 GIT_REPOSITORY      https://github.com/henryiii/CLI11.git
+		         GIT_TAG             master
+                 UPDATE_DISCONNECTED 1
+                 QUIET
+)
+
+add_subdirectory(${cli_SOURCE_DIR} ${cli_SOURCE_DIR})
+

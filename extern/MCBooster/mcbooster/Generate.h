5efc6f9d375d04ac1e07dd245ff5724a7938d0f9
Henry Schreiner <hschrein@cern.ch>
2017-05-05 10:29:35 -0400

    Fix for cuda-affecting error in rename

--- extern/MCBooster/mcbooster/Generate.h
+++ extern/MCBooster/mcbooster/Generate.h
@@ -74,7 +74,7 @@
 	cudaError_t _m_cudaStat = value;										\
 	if (_m_cudaStat != cudaSuccess) {										\
 		fprintf(stderr, "Error %s at line %d in file %s\n",					\
-				cudagetErrorString(_m_cudaStat), __LINE__, __FILE__);		\
+				cudaGetErrorString(_m_cudaStat), __LINE__, __FILE__);		\
 		exit(1);															\
 	} }
 

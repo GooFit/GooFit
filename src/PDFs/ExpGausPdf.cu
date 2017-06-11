5d7e26d3a5570c37c5170bb898fb17d5e1dcb036
Bradley Hittle <hittle.6@osu.edu>
2017-06-11 16:20:34 -0400

    Added changes for ExpGaussPdf with test.  Removing mislocated files from merge.

--- src/PDFs/ExpGausPdf.cu
+++ src/PDFs/ExpGausPdf.cu
@@ -2,11 +2,12 @@
 
 namespace GooFit {
 
-__device__ fptype device_ExpGaus(fptype *evt, fptype *p, unsigned int *indices) {
-    fptype x     = evt[RO_CACHE(indices[2 + RO_CACHE(indices[0])])];
-    fptype mean  = RO_CACHE(p[RO_CACHE(indices[1])]);
-    fptype sigma = RO_CACHE(p[RO_CACHE(indices[2])]);
-    fptype alpha = RO_CACHE(p[RO_CACHE(indices[3])]);
+__device__ fptype device_ExpGaus(fptype *evt, ParameterContainer &pc) {
+    int id = pc.constants[pc.constantIdx + 1];
+    fptype x     = evt[id];
+    fptype mean  = RO_CACHE(pc.parameters[pc.parameterIdx + 1]);
+    fptype sigma = RO_CACHE(pc.parameters[pc.parameterIdx + 2]);
+    fptype alpha = RO_CACHE(pc.parameters[pc.parameterIdx + 3]);
 
     fptype ret    = 0.5 * alpha;
     fptype exparg = ret * (2 * mean + alpha * sigma * sigma - 2 * x);
@@ -14,6 +15,8 @@ __device__ fptype device_ExpGaus(fptype *evt, fptype *p, unsigned int *indices) 
 
     ret *= exp(exparg);
     ret *= erfc(erfarg);
+
+    pc.incrementIndex (1, 3, 1, 0, 1);
 
     return ret;
 }
@@ -23,11 +26,26 @@ __device__ device_function_ptr ptr_to_ExpGaus = device_ExpGaus;
 ExpGausPdf::ExpGausPdf(std::string n, Variable *_x, Variable *mean, Variable *sigma, Variable *tau)
     : GooPdf(_x, n) {
     std::vector<unsigned int> pindices;
+
+    //reserving index for _x    
+    constantsList.push_back (0);
+
     pindices.push_back(registerParameter(mean));
     pindices.push_back(registerParameter(sigma));
     pindices.push_back(registerParameter(tau));
     GET_FUNCTION_ADDR(ptr_to_ExpGaus);
     initialize(pindices);
+}
+
+__host__ void ExpGausPdf::recursiveSetIndices () {
+    GET_FUNCTION_ADDR(ptr_to_ExpGaus);
+
+    GOOFIT_TRACE("host_function_table[{}] = {}({})", num_device_functions, getName (), "ptr_to_ExpGaus");
+    host_function_table[num_device_functions] = host_fcn_ptr;
+    functionIdx = num_device_functions++;
+
+    populateArrays ();
+ 
 }
 
 } // namespace GooFit

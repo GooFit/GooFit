57bc84d5a9dc1481d85e4cfb53bb8ad7a8bd8361
Himadri Pandey <pandeyhs@mail.uc.edu>
2017-06-20 01:58:20 -0400

    added vector pdfs and updated cmake and goofit file

---
+++ python/PDFs/basic/PolynomialPdf.cpp
@@ -0,0 +1,16 @@
+#include <pybind11/pybind11.h>
+
+#include <goofit/Variable.h>
+#include <goofit/PDFs/basic/PolynomialPdf.h>
+
+using namespace GooFit;
+namespace py = pybind11;
+
+void init_PolynomialPdf(py::module &m) {
+    py::class_<PolynomialPdf, GooPdf>(m, "PolynomialPdf")
+        .def(py::init<std::string, Variable *, std::vector<Variable *>>())
+        .def(py::init<std::string, Variable *, std::vector<Variable *>, Variable *, unsigned int >())
+        .def(py::init<std::string,std::vector<Variable *>, std::vector<Variable *>, std::vector<Variable *>>())
+        .def(py::init<std::string,std::vector<Variable *>, std::vector<Variable *>, std::vector<Variable *>, unsigned int>())
+        ;
+}
--- python/goofit.cpp
+++ python/goofit.cpp
@@ -8,7 +8,25 @@ void init_UnbinnedDataSet(py::module &);
 void init_Variable(py::module &);
 void init_PdfBase(py::module &);
 void init_GooPdf(py::module &);
-void init_ExpPdf(py::module &);
+void init_ArgusPdf(py::module &);
+void init_BifurGaussPdf(py::module &);
+void init_BinTransformPdf(py::module &);
+void init_BWPdf(py::module &);
+void init_CorrGaussianPdf(py::module &);
+void init_CrystalBallPdf(py::module &);
+void init_ExpGausPdf(py::module &);
+void init_GaussianPdf(py::module &);
+void init_InterHistPdf(py::module &);
+void init_JohnsonSUPdf(py::module &);
+void init_KinLimitBWPdf(py::module &);
+void init_LandauPdf(py::module &);
+void init_NovosibirskPdf(py::module &);
+void init_PolynomialPdf(py::module &);
+void init_ScaledGaussianPdf(py::module &);
+void init_SmoothHistogramPdf(py::module &);
+void init_StepPdf(py::module &);
+void init_TrigThresholdPdf(py::module &);
+void init_VoigtianPdf(py::module &);
 void init_FitManager(py::module &);
 
 PYBIND11_PLUGIN(goofit) {

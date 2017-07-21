32accda20ea913839a8b076e01d82e24cae230a3
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-07-21 16:51:12 -0400

    Adding fitTo shortcut to Python, as well

--- python/goofit/PdfBase.cpp
+++ python/goofit/PdfBase.cpp
@@ -12,5 +12,7 @@ using namespace GooFit;
 void init_PdfBase(py::module &m) {
     py::class_<PdfBase>(m, "PdfBase")
         .def("setData", (void (PdfBase::*)(DataSet *)) & PdfBase::setData)
-        .def("setData", (void (PdfBase::*)(std::vector<std::map<Variable *, fptype>> &)) & PdfBase::setData);
+        .def("setData", (void (PdfBase::*)(std::vector<std::map<Variable *, fptype>> &)) & PdfBase::setData)
+        .def("fitTo", &PdfBase::fitTo)
+    ;
 }

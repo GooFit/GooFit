c9345a69701bb456dae99901b366c89021a879a0
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-08-01 16:53:33 -0500

    Nicer readme

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 try:
     from skbuild import setup
 except ImportError:
-    print("Failed to find scikit-build, please run `pip install scikit-build cmake`)
+    print("Failed to find scikit-build, please run `pip install scikit-build cmake`")
     raise
 
 setup(
         name='goofit',
-        version='2.1.0.dev1',
+        version='2.1.0.dev2',
         description='GooFit fitting package',
         author='Henry Schreiner',
         author_email='hschrein@cern.ch',
@@ -20,26 +20,29 @@ setup(
         license="LGPL 3.0",
         packages=['goofit'],
         long_description='''\
-# GooFit for Python
+GooFit for Python
+-----------------
 
 GooFit is a highly parallel fitting framework originally designed for High Energy Physics.
 
-## Installation
+Installation
+============
 
-This package can be installed with pip, but uses SciKit-Build, and is build, fully optimized, on your system. Because of this, there are a few caveats when running a pip install. Make sure you have SciKit-Build (`pip install scikit-build`) before you attempt an install. Also, if you don't have a recent version of CMake (3.8 or better recommended), also run `pip install cmake`. When you build, you should also use pip's `-v` flag, so that you can see it build (and observe the
+This package can be installed with pip, but uses SciKit-Build, and is build, fully optimized, on your system. Because of this, there are a few caveats when running a pip install. Make sure you have SciKit-Build (``pip install scikit-build``) before you attempt an install. Also, if you don't have a recent version of CMake (3.8 or better recommended), also run ``pip install cmake``. When you build, you should also use pip's ``-v`` flag, so that you can see it build (and observe the
 configuration options). Otherwise, you might wait a very long time without output (especially if CUDA was found).
 
-In practice, this looks like this:
+In practice, this looks like this::
 
     pip install scikit-build cmake
     pip install -v goofit
 
 
-## Building a source package from git
+Building a source package from git
+==================================
 
 For developers:
 
-To make a source package, start with a clean (such as new) git GooFit package with all submodules checked out.
+To make a source package, start with a clean (such as new) git GooFit package with all submodules checked out::
 
     git clone --branch=master --recursive --depth=10 git@github.com:GooFit/GooFit.git
     cd goofit

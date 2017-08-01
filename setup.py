a5b8e705c93373b20e3e82051fd0bd7fec00cd59
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-08-01 15:59:03 -0500

    Adding start of manifest, dev1 version

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from skbuild import setup
 
 setup(
         name='goofit',
-        version='2.1',
+        version='2.1.0.dev1',
         description='GooFit fitting package',
         author='Henry Schreiner',
         author_email='hschrein@cern.ch',
         url='https://goofit.github.io',
         platforms = ["POSIX"],
         provides = ["goofit"],
-        cmake_args=['-DGOOFIT_PYTHON:BOOL=ON'],
+        cmake_args=[
+            '-DGOOFIT_PYTHON=ON',
+            '-DGOOFIT_EXAMPLES=OFF'],
         license="LGPL 3.0",
         packages=['goofit']
-        # description="A parallel fitting package."
         )
 

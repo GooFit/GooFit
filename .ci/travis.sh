dfae3938e8fed2a7599823193797fc5c6c95e562
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-08-02 09:42:35 -0500

    Removing python build from stable branch

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -5,10 +5,9 @@ cd ${TRAVIS_BUILD_DIR}
 
 mkdir build || true
 cd build
-cmake -DGOOFIT_DEVICE=OMP -DGOOFIT_SEPARATE_COMP=ON -DGOOFIT_TESTS=ON -DCMAKE_BUILD_TYPE=Coverage -DGOOFIT_PYTHON=ON ..
+cmake -DGOOFIT_DEVICE=OMP -DGOOFIT_SEPARATE_COMP=ON -DGOOFIT_TESTS=ON -DCMAKE_BUILD_TYPE=Coverage ..
 cmake --build . -- -j2
 cmake --build . --target GooFit_coverage
-pytest
 
 lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '*/tests/*' '*gtest*' '*gmock*' '/usr/*' --output-file coverage.info # filter out system

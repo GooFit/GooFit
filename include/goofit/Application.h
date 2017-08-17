96d60310604b8d0191f701047d6dd8366a39b807
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-08-17 11:29:37 -0400

    Nicer splash

--- include/goofit/Application.h
+++ include/goofit/Application.h
@@ -44,34 +44,37 @@ using CLI::ExitCodes;
 
 /// Optional print for splash (not used by default in Application for brevity)
 /// Slant Relief from http://patorjk.com/software/taag/#p=testall&f=Wavy&t=GooFit (tightened a bit)
-void print_splash(std::ostream &out) {
-    out << std::endl <<  reset << green << "                                     Welcome to";
+void print_splash() {
+    std::cout << std::endl <<  reset << green << "       Welcome to";
     std::string splash = R"raw(
-  _____/\\\\\\\\\\\\______________________________/\\\\\\\\\\\\____________________
-  ___/\\\//////////______________________________\/\\\/////////____________________
-  __/\\\_________________________________________\/\\\__________/\\\_____/\\\______
-  _\/\\\____/\\\\\\\_____/\\\\\________/\\\\\____\/\\\\\\\\\\__\////__/\\\\\\\\\\\_
-  _\/\\\___\/////\\\___/\\\///\\\____/\\\///\\\__\/\\\///////___/\\\_\////\\\////__
-  _\/\\\_______\/\\\__/\\\__\//\\\__/\\\__\//\\\_\/\\\_________\/\\\____\/\\\______
-  _\/\\\_______\/\\\_\//\\\__/\\\__\//\\\__/\\\__\/\\\_________\/\\\____\/\\\_/\\__
-  _\//\\\\\\\\\\\\/___\///\\\\\/____\///\\\\\/___\/\\\_________\/\\\____\//\\\\\___
-  __\////////////_______\/////________\/////_____\///__________\///______\/////____
-    )raw";
+   ██████╗  ██████╗  ██████╗ ███████╗██╗████████╗
+  ██╔════╝ ██╔═══██╗██╔═══██╗██╔════╝██║╚══██╔══╝
+  ██║  ███╗██║   ██║██║   ██║█████╗  ██║   ██║
+  ██║   ██║██║   ██║██║   ██║██╔══╝  ██║   ██║
+  ╚██████╔╝╚██████╔╝╚██████╔╝██║     ██║   ██║
+   ╚═════╝  ╚═════╝  ╚═════╝ ╚═╝     ╚═╝   ╚═╝
+)raw";
     
-    out << reset << dim;
-    bool cur = false;
-    for(auto c : splash) {
-        if(c!='_' && !cur) {
-            out << reset << green;
-            cur = true;
-        } else if(c=='_' && cur) {
-            out << reset << dim;
-            cur = false;
+    std::cout << reset << dim;
+    bool cur_green = false;
+    for(int i=0; i<splash.size(); i++) {
+        std::string letter = splash.substr(i,3);
+        bool is_edge = letter=="╚" || letter=="╝" || letter=="╗" || letter=="╔" || letter=="║" || letter=="═";
+        bool is_block = letter=="█";
+        
+        if(is_block && !cur_green) {
+            std::cout << reset << green;
+            cur_green = true;
+        } else if(is_edge && cur_green) {
+            std::cout << reset << dim;
+            cur_green = false;
         }
-        out << c;
+        std::cout << splash[i];
     }
-    out << reset << std::flush;
+    
+    std::cout << reset << std::flush;
 }
+    
 class Application : public CLI::App {
   protected:
     int gpuDev_ = 0;
@@ -170,6 +173,9 @@ class Application : public CLI::App {
         set_device();
 
         if(!quiet_) {
+            print_splash();
+            std::cout << std::endl;
+            
             GOOFIT_INFO("GooFit: Version {} ({}) Commit: {}", GOOFIT_VERSION, GOOFIT_TAG, GOOFIT_GIT_VERSION);
 
 #if THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA

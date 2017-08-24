5451efa5185f8f7e94f763a061a8e408741959d7
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-08-24 11:25:37 -0700

    Starting to use Macros

--- include/goofit/detail/Macros.h
+++ include/goofit/detail/Macros.h
@@ -1,23 +1,43 @@
 #pragma once
 
+#include "../Log.h"
+
 // The following macros help with registering new values
 
 
-#define GOOFIT_ADD_PARAM(pindices,par) \
-    (pindices).push_back(registerParameter((par));
+#define GOOFIT_START_PDF \
+    std::vector<unsigned int> pindices; \
+    pindices.push_back(0);
 
-#define GOOFIT_ADD_OBS(par) \
-    registarObservable((par));
+#define GOOFIT_ADD_PARAM(i, par, name) { \
+    pindices.push_back(registerParameter((par))); \
+    if((i) != pindices.size()) \
+        throw GooFit::GeneralError("{}: Param {} {} actually has number {}", \
+                getName(), (name), (i), pindices.size()); }
 
-#define GOOFIT_ADD_CONST(pindices, par, name) \
-            (pindices).push_back(registerConstants); \
-            MEMCPY_TO_SYMBOL(functorConstants, &(par), sizeof par, cIndex*sizeof par, cudaMemcpyHostToDevice); \
-            GOOFIT_DEBUG("{}: Registered constant value {}={} at c:{}", getName(), (name), (par), cIndex); \
+#define GOOFIT_ADD_OBS(par) { \
+    registarObservable((par)) } ;
 
-#define GOOFIT_ADD_INT(pindices, par, name) \
-            GOOFIT_DEBUG("{}: Registered integer {} at {}", getName(), (name),  (par), (pindices).size()); \
-            (pindices).push_back((par));
+#define GOOFIT_ADD_CONST(i, par, name) { \
+    pindices.push_back(registerConstants(1)); \
+    MEMCPY_TO_SYMBOL(functorConstants, &(par), sizeof par, cIndex*sizeof par, cudaMemcpyHostToDevice); \
+    if((i) != cIndex) \
+        throw GooFit::GeneralError("{}: Param {} {} actually has number {}", \
+            getName(), (name), (i), cIndex); \
+    GOOFIT_DEBUG("{}: Registered constant value {}={} at c:{}", getName(), (name), (par), pindices.size()); } 
 
-#define GOOFIT_FINALIZE_PARAM(pindices) \
-    initialize((pindices)); \
+#define GOOFIT_ADD_INT(i, par, name) { \
+    GOOFIT_DEBUG("{}: Registered integer {} at {}", getName(), (name),  (par), pindices.size()); \
+    pindices.push_back((par)); \
+    if((i) != cIndex) \
+        throw GooFit::GeneralError("{}: Param {} {} actually has number {}", \
+            getName(), (name), (i), cIndex); } 
+
+#define GOOFIT_FINALIZE_PDF \
+    initialize(pindices); \
     GOOFIT_DEBUG("{}: Initializing indices", getName());
+
+#define GOOFIT_GET_PARAM(i) cudaArray[indices[(i)]]
+#define GOOFIT_GET_INT(i) indices[(i)]
+#define GOOFIT_GET_CONST(i) functorConstants[indices[(i)]]
+

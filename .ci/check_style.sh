619c8967c2ab51cbc833f0006e51c3e284c22136
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-09-01 14:19:16 -0400

    Adding clang-tidy fix

--- .ci/check_style.sh
+++ .ci/check_style.sh
@@ -6,3 +6,12 @@ clang-format --version
 git ls-files -- '*.cpp' '*.hpp' '*.cu' '*.h' '*.cc' | xargs clang-format -i -style=file
 
 git diff --exit-code --color
+
+mkdir build || true
+cd build
+cmake .. -DGOOFIT_TIDY_FIX=ON
+cmake --build .
+
+git diff --exit-code --color
+
+set +evx

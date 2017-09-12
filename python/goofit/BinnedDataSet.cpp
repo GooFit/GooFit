8e8bc4a067c0b0550c4d33e52a4bebf3b700a404
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-09-12 07:43:22 -0400

    Update to new init over placement new

--- python/goofit/BinnedDataSet.cpp
+++ python/goofit/BinnedDataSet.cpp
@@ -8,16 +8,16 @@ namespace py = pybind11;
 
 void init_BinnedDataSet(py::module &m) {
     py::class_<BinnedDataSet, DataSet>(m, "BinnedDataSet")
-        .def("__init__",
-             [](BinnedDataSet &instance, py::args args, py::kwargs kwargs) {
+        .def(py::init(
+             [](py::args args, py::kwargs kwargs) {
                  std::string name;
                  std::vector<Variable *> vars;
                  for(auto arg : args)
                      vars.push_back(arg.cast<Variable *>());
                  if(kwargs.contains("name"))
                      name = kwargs["name"].cast<std::string>();
-                 new(&instance) BinnedDataSet(vars, name);
-             })
+                 return new BinnedDataSet(vars, name);
+             }))
         .def("getBinCenter", (fptype(BinnedDataSet::*)(size_t, size_t) const) & BinnedDataSet::getBinCenter)
         .def("getBinNumber", &BinnedDataSet::getBinNumber)
         .def("getBinVolume", &BinnedDataSet::getBinVolume)

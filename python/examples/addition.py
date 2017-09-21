a94785d80c846dc81ea687d44bcc59e07ba68a71
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-09-21 13:12:38 -0400

    Fix addition.py

--- python/examples/addition.py
+++ python/examples/addition.py
@@ -14,14 +14,14 @@ i=-1
 
 while i < 100000:
     i+=1
-    xvar.value = np.random.normal(0.2,1.1) 
-    
+    xvar.value = np.random.normal(0.2,1.1)
+
     if np.random.uniform() < 0.1:
         xvar.value = np.random.uniform(-5,5)
 
 
     if abs(xvar.value) > 5:
-        --i
+        i-=1
         continue
 
 
@@ -34,13 +34,12 @@ xsigm = Variable("xsigm", 1, 0.5, 1.5)
 signal = GaussianPdf("signal", xvar, xmean, xsigm)
 
 
-constant = vector(Variable("constant", 1.0))
+constant = [Variable("constant", 1.0)]
 backgr = PolynomialPdf("backgr", xvar, constant)
 
 sigFrac = Variable("sigFrac", 0.9, 0.75, 1.00)
 
-comps = PdfBase(signal,backgr)
-total = AddPdf("total", sigFrac, comps)
+total = AddPdf("total", [sigFrac], [signal,backgr])
 total.setData(data)
 
 fitter = FitManager(total)

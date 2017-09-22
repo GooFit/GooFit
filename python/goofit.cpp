c6481131b82e966649e19cbea21797ba671fecd9
Henry Schreiner <henryschreineriii@utexas.edu>
2017-09-22 07:11:10 -0400

    Beginning Minuit wrapper (#115)

--- python/goofit.cpp
+++ python/goofit.cpp
@@ -2,6 +2,8 @@
 
 namespace py = pybind11;
 
+void init_FunctionMinimum(py::module &);
+
 void init_DataSet(py::module &);
 void init_BinnedDataSet(py::module &);
 void init_UnbinnedDataSet(py::module &);
@@ -61,6 +63,8 @@ void init_VariableBinTransform1DPdf(py::module &);
 
 PYBIND11_MODULE(_goofit, m) {
     m.doc() = "Python interface for GooFit";
+
+    init_FunctionMinimum(m);
 
     init_Variable(m);
     init_DataSet(m);

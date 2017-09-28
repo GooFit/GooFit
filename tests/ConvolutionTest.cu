c9b41adaae03b2b78935c6722ca681e56af0a829
Bradley Hittle <hittle.6@osu.edu>
2017-09-28 11:51:45 -0400

    Convolution test

---
+++ tests/ConvolutionTest.cu
@@ -0,0 +1,62 @@
+#include "gtest/gtest.h"
+
+#include "goofit/fitting/FitManagerMinuit1.h"
+#include "goofit/UnbinnedDataSet.h"
+#include "goofit/PDFs/combine/ConvolutionPdf.h"
+#include "goofit/PDFs/basic/GaussianPdf.h"
+
+#include "goofit/Variable.h"
+
+#include <sys/time.h>
+#include <sys/times.h>
+#include <iostream>
+
+#include <random>
+
+using namespace std;
+using namespace GooFit;
+
+TEST(Convolution, SimpleFit) {
+    // Random number generation
+    std::mt19937 gen(137);
+    std::exponential_distribution<> d(1.5);
+
+    // Independent variable.
+    Variable xvar{"xvar", 0, 10};
+
+    // Data set
+    UnbinnedDataSet data(&xvar);
+
+    // Generate toy events.
+    for(int i = 0; i < 1000; ++i) {
+        double val = d(gen);
+        if(val < 10 && val > 0) {
+            xvar.setValue(val);
+            data.addEvent();
+        }
+    }
+
+    // Fit parameter
+    Variable a1{"alpha1", 1, 0.1, -10, 10};
+    Variable s1{"sigma1", 1, 0.1, 3};
+    Variable a2{"alpha2", 1, 0.1, -10, 10};
+    Variable s2{"sigma2", 1, 0.1, 3};
+
+    GaussianPdf m{"gauss1", &xvar, &a1, &s1};
+    GaussianPdf r{"gauss2", &xvar, &a2, &s2};
+
+    // GooPdf object
+    ConvolutionPdf pdf{"convolutionpdf", &xvar, &m, &r};
+    pdf.setData(&data);
+
+    GooFit::FitManagerMinuit1 fitter{&pdf};
+    fitter.setVerbosity(2);
+    fitter.fit();
+
+    EXPECT_TRUE(fitter);
+    EXPECT_LT(a1.getError(), .1);
+    EXPECT_NEAR(0.665178392, a1.getValue(), a1.getError() * 3);
+    EXPECT_LT(a2.getError(), .1);
+    EXPECT_NEAR(0.665178392, a2.getValue(), a2.getError() * 3);
+}
+

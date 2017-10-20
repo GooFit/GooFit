59c7a6c948bdb3387281feb0648468970a30e64e
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-10-20 15:28:35 -0400

    Move to using seperate classes for Resonances

--- python/PDFs/physics/ResonancePdf.cu
+++ python/PDFs/physics/ResonancePdf.cu
@@ -10,19 +10,12 @@ using namespace pybind11::literals;
 void init_ResonancePdf(py::module &m) {
     m.attr("MAXNKNOBS") = MAXNKNOBS;
 
-    py::enum_<ResPdfType>(m, "ResPdfType", py::arithmetic())
-        .value("RBW", ResPdfType::RBW)
-        .value("LASS", ResPdfType::LASS)
-        .value("GS", ResPdfType::GS)
-        .value("FLATTE", ResPdfType::FLATTE)
-        .value("GAUSS", ResPdfType::GAUSS)
-        .value("SPLINE", ResPdfType::SPLINE)
-        .value("NONRES", ResPdfType::NONRES)
-        .export_values();
-
-    py::class_<ResonancePdf, GooPdf>(m, "ResonancePdf")
+    auto m_ls = m.def_submodule("Lineshapes");
+    
+    py::class_<ResonancePdf, GooPdf>(m, "ResonancePdf");
+    
+    py::class_<Resonances::RBW, ResonancePdf>(m_ls, "RBW")
         .def(py::init<std::string,
-                      ResPdfType,
                       Variable *,
                       Variable *,
                       Variable *,
@@ -30,9 +23,8 @@ void init_ResonancePdf(py::module &m) {
                       unsigned int,
                       unsigned int,
                       bool>(),
-             "Constructor for regular BW,Gounaris-Sakurai,LASS",
+             "Constructor for regular BW",
              "name"_a,
-             "ResPdfType"_a,
              "ar"_a,
              "ai"_a,
              "mass"_a,
@@ -40,27 +32,67 @@ void init_ResonancePdf(py::module &m) {
              "sp"_a,
              "cyc"_a,
              "symmDP"_a = false)
+    ;
+    
+    py::class_<Resonances::GS, ResonancePdf>(m, "GS")
+    .def(py::init<std::string,
+         Variable *,
+         Variable *,
+         Variable *,
+         Variable *,
+         unsigned int,
+         unsigned int,
+         bool>(),
+         "Constructor for regular Gounaris-Sakurai",
+         "name"_a,
+         "ar"_a,
+         "ai"_a,
+         "mass"_a,
+         "width"_a,
+         "sp"_a,
+         "cyc"_a,
+         "symmDP"_a = false)
+    ;
 
-        .def(py::init<std::string, ResPdfType, Variable *, Variable *>(),
-             "Constructor for regular BW,Gounaris-Sakurai,LASS"
-             "Constructor for NONRES",
+    py::class_<Resonances::LASS, ResonancePdf>(m, "LASS")
+    .def(py::init<std::string,
+         Variable *,
+         Variable *,
+         Variable *,
+         Variable *,
+         unsigned int,
+         unsigned int,
+         bool>(),
+         "Constructor for LASS",
+         "name"_a,
+         "ar"_a,
+         "ai"_a,
+         "mass"_a,
+         "width"_a,
+         "sp"_a,
+         "cyc"_a,
+         "symmDP"_a = false)
+    ;
+    
+    py::class_<Resonances::NonRes, ResonancePdf>(m, "NonRes")
+        .def(py::init<std::string, Variable *, Variable *>(),
+             "Constructor for NonResonant",
              "name"_a,
-             "ResPdfType"_a,
              "ar"_a,
-             "ai"_a)
+             "ai"_a);
 
-        .def(py::init<std::string, ResPdfType, Variable *, Variable *, Variable *, Variable *, unsigned int>(),
+    py::class_<Resonances::Gauss, ResonancePdf>(m, "Gauss")
+        .def(py::init<std::string, Variable *, Variable *, Variable *, Variable *, unsigned int>(),
              "Constructor for regular GAUSS",
              "name"_a,
-             "ResPdfType"_a,
              "ar"_a,
              "ai"_a,
              "mean"_a,
              "sigma"_a,
-             "cyc"_a)
+             "cyc"_a);
 
+    py::class_<Resonances::FLATTE, ResonancePdf>(m, "FLATTE")
         .def(py::init<std::string,
-                      ResPdfType,
                       Variable *,
                       Variable *,
                       Variable *,
@@ -70,17 +102,16 @@ void init_ResonancePdf(py::module &m) {
                       bool>(),
              "Constructor for regular FLATTE",
              "name"_a,
-             "ResPdfType"_a,
              "ar"_a,
              "ai"_a,
              "mean"_a,
              "g1"_a,
              "rg2og1"_a,
              "cyc"_a,
-             "symmDP"_a)
+             "symmDP"_a);
 
+    py::class_<Resonances::Spline, ResonancePdf>(m, "Spline")
         .def(py::init<std::string,
-                      ResPdfType,
                       Variable *,
                       Variable *,
                       std::vector<fptype> &,
@@ -88,9 +119,8 @@ void init_ResonancePdf(py::module &m) {
                       std::vector<Variable *> &,
                       unsigned int,
                       bool>(),
-             "Constructor for regular CUBIC spline",
+             "Constructor for regular cubic spline",
              "name"_a,
-             "ResPdfType"_a,
              "ar"_a,
              "ai"_a,
              "HH_bin_limits"_a,
--- src/PDFs/physics/ResonancePdf.cu
+++ src/PDFs/physics/ResonancePdf.cu
@@ -470,9 +470,11 @@ __device__ resonance_function_ptr ptr_to_LASS     = lass;
 __device__ resonance_function_ptr ptr_to_FLATTE   = flatte;
 __device__ resonance_function_ptr ptr_to_SPLINE   = cubicspline;
 
-// Constructor for regular BW,Gounaris-Sakurai,LASS
-ResonancePdf::ResonancePdf(std::string name,
-                           ResPdfType rpt,
+    
+namespace Resonances {
+
+
+RBW::RBW(std::string name,
                            Variable *ar,
                            Variable *ai,
                            Variable *mass,
@@ -480,173 +482,174 @@ ResonancePdf::ResonancePdf(std::string name,
                            unsigned int sp,
                            unsigned int cyc,
                            bool symmDP)
-    : GooPdf(nullptr, name)
-    , amp_real(ar)
-    , amp_imag(ai)
-    , rpt_(rpt) {
-    // Making room for index of decay-related constants. Assumption:
-    // These are mother mass and three daughter masses in that order.
-    // They will be registered by the object that uses this resonance,
-    // which will tell this object where to find them by calling setConstantIndex.
+: ResonancePdf(name, ar, ai) {
 
-    std::vector<unsigned int> pindices;
-    pindices.push_back(0);
+    
     pindices.push_back(registerParameter(mass));
     pindices.push_back(registerParameter(width));
     pindices.push_back(sp);
     pindices.push_back(cyc);
     pindices.push_back((unsigned int)symmDP);
+    
+    GET_FUNCTION_ADDR(ptr_to_RBW);
 
-    if(rpt_ == ResPdfType::RBW) {
-        GET_FUNCTION_ADDR(ptr_to_RBW);
-    } else if(rpt_ == ResPdfType::GS) {
+    initialize(pindices);
+}
+    
+    GS::GS(std::string name,
+             Variable *ar,
+             Variable *ai,
+             Variable *mass,
+             Variable *width,
+             unsigned int sp,
+             unsigned int cyc,
+             bool symmDP)
+    : ResonancePdf(name, ar, ai) {
+        
+        
+        pindices.push_back(registerParameter(mass));
+        pindices.push_back(registerParameter(width));
+        pindices.push_back(sp);
+        pindices.push_back(cyc);
+        pindices.push_back((unsigned int)symmDP);
+        
         GET_FUNCTION_ADDR(ptr_to_GOUSAK);
-    } else if(rpt_ == ResPdfType::LASS) {
+        
+        initialize(pindices);
+    }
+    
+    LASS::LASS(std::string name,
+             Variable *ar,
+             Variable *ai,
+             Variable *mass,
+             Variable *width,
+             unsigned int sp,
+             unsigned int cyc,
+             bool symmDP)
+    : ResonancePdf(name, ar, ai) {
+        
+        
+        pindices.push_back(registerParameter(mass));
+        pindices.push_back(registerParameter(width));
+        pindices.push_back(sp);
+        pindices.push_back(cyc);
+        pindices.push_back((unsigned int)symmDP);
+        
         GET_FUNCTION_ADDR(ptr_to_LASS);
-    } else
-        throw GeneralError("Wrong constructor for the reqested ResPdfType, this is the RBW/GS/LASS constructor");
-
-    initialize(pindices);
-}
-
+        
+        initialize(pindices);
+    }
+    
+    
 // Constructor for regular BW,Gounaris-Sakurai,LASS
-ResonancePdf::ResonancePdf(
-    std::string name, ResPdfType rpt, Variable *ar, Variable *ai, Variable *mass, Variable *width, unsigned int cyc)
-    : GooPdf(nullptr, name)
-    , amp_real(ar)
-    , amp_imag(ai)
-    , rpt_(rpt) {
-    // Making room for index of decay-related constants. Assumption:
-    // These are mother mass and three daughter masses in that order.
-    // They will be registered by the object that uses this resonance,
-    // which will tell this object where to find them by calling setConstantIndex.
-
-    std::vector<unsigned int> pindices;
-    pindices.push_back(0);
-    pindices.push_back(registerParameter(mass));
-    pindices.push_back(registerParameter(width));
-    pindices.push_back(cyc);
-
-    if(rpt_ == ResPdfType::GAUSS) {
+    Gauss::Gauss(std::string name, Variable *ar, Variable *ai, Variable *mass, Variable *width, unsigned int cyc)
+    : ResonancePdf(name, ar, ai) {
+        // Making room for index of decay-related constants. Assumption:
+        // These are mother mass and three daughter masses in that order.
+        // They will be registered by the object that uses this resonance,
+        // which will tell this object where to find them by calling setConstantIndex.
+        
+        std::vector<unsigned int> pindices;
+        pindices.push_back(0);
+        pindices.push_back(registerParameter(mass));
+        pindices.push_back(registerParameter(width));
+        pindices.push_back(cyc);
+        
         GET_FUNCTION_ADDR(ptr_to_GAUSSIAN);
-    } else
-        throw GeneralError("Wrong constructor for the reqested ResPdfType, this is the GAUSS constructor");
-
-    initialize(pindices);
-}
-
-ResonancePdf::ResonancePdf(std::string name, ResPdfType rpt, Variable *ar, Variable *ai)
-    : GooPdf(nullptr, name)
-    , amp_real(ar)
-    , amp_imag(ai)
-    , rpt_(rpt) {
-    // Dummy index for constants - won't use it, but calling
-    // functions can't know that and will call setConstantIndex anyway.
-    std::vector<unsigned int> pindices;
-    pindices.push_back(0);
-
-    if(rpt_ == ResPdfType::NONRES) {
+        
+        initialize(pindices);
+    }
+    
+    NonRes::NonRes(std::string name, Variable *ar, Variable *ai)
+    : ResonancePdf(name, ar, ai) {
+ 
         GET_FUNCTION_ADDR(ptr_to_NONRES);
-    } else
-        throw GeneralError("Wrong constructor for the reqested ResPdfType, this is the NONRES constructor");
-
-    initialize(pindices);
-}
-
-ResonancePdf::ResonancePdf(std::string name,
-                           ResPdfType rpt,
-                           Variable *ar,
-                           Variable *ai,
-                           Variable *mean,
-                           Variable *g1,
-                           Variable *rg2og1,
-                           unsigned int cyc,
-                           bool symmDP)
-    : GooPdf(nullptr, name)
-    , amp_real(ar)
-    , amp_imag(ai)
-    , rpt_(rpt) {
-    if(rpt_ != ResPdfType::FLATTE)
-        throw GeneralError("Wrong constructor for the reqested ResPdfType, this is the FLATTE constructor");
-
-    std::vector<unsigned int> pindices;
-    pindices.push_back(0);
-
-    // Dummy index for constants - won't use it, but calling
-    // functions can't know that and will call setConstantIndex anyway.
-
-    pindices.push_back(registerParameter(mean));
-    pindices.push_back(registerParameter(g1));
-    pindices.push_back(registerParameter(rg2og1));
-    pindices.push_back(cyc);
-    pindices.push_back((unsigned int)symmDP);
-
-    GET_FUNCTION_ADDR(ptr_to_FLATTE);
-
-    initialize(pindices);
-}
-
-ResonancePdf::ResonancePdf(std::string name,
-                           ResPdfType rpt,
-                           Variable *ar,
-                           Variable *ai,
-                           std::vector<fptype> &HH_bin_limits,
-                           std::vector<Variable *> &pwa_coefs_reals,
-                           std::vector<Variable *> &pwa_coefs_imags,
-                           unsigned int cyc,
-                           const bool symmDP)
-    : GooPdf(nullptr, name)
-    , amp_real(ar)
-    , amp_imag(ai)
-    , rpt_(rpt) {
-    if(rpt_ != ResPdfType::SPLINE)
-        throw GeneralError("Wrong constructor for the reqested ResPdfType, this is the CUBIC constructor");
-
-    std::vector<unsigned int> pindices;
-    const unsigned int nKnobs = HH_bin_limits.size();
-    host_constants.resize(nKnobs);
-
-    pindices.push_back(0);
-    pindices.push_back(cyc);
-    pindices.push_back((unsigned int)symmDP);
-    pindices.push_back(nKnobs);
-
-    for(int i = 0; i < pwa_coefs_reals.size(); i++) {
-        host_constants[i] = HH_bin_limits[i];
-        pindices.push_back(registerParameter(pwa_coefs_reals[i]));
-        pindices.push_back(registerParameter(pwa_coefs_imags[i]));
+        
+        initialize(pindices);
     }
-    pindices.push_back(registerConstants(nKnobs));
-
-    MEMCPY_TO_SYMBOL(functorConstants,
-                     host_constants.data(),
-                     nKnobs * sizeof(fptype),
-                     cIndex * sizeof(fptype),
-                     cudaMemcpyHostToDevice);
-
-    GET_FUNCTION_ADDR(ptr_to_SPLINE);
-
-    initialize(pindices);
-}
-
-__host__ void ResonancePdf::recalculateCache() const {
-    if(rpt_ == ResPdfType::SPLINE) {
-        auto params           = getParameters();
-        const unsigned nKnobs = params.size() / 2;
-        std::vector<fpcomplex> y(nKnobs);
-        unsigned int i = 0;
-        for(auto v = params.begin(); v != params.end(); ++v, ++i) {
-            unsigned int idx = i / 2;
-            fptype value     = host_params[(*v)->getIndex()];
-            if(i % 2 == 0)
-                y[idx].real(value);
-            else
-                y[idx].imag(value);
+    
+    FLATTE::FLATTE(std::string name,
+                               Variable *ar,
+                               Variable *ai,
+                               Variable *mean,
+                               Variable *g1,
+                               Variable *rg2og1,
+                               unsigned int cyc,
+                               bool symmDP)
+    : ResonancePdf(name, ar, ai) {
+        
+        pindices.push_back(registerParameter(mean));
+        pindices.push_back(registerParameter(g1));
+        pindices.push_back(registerParameter(rg2og1));
+        pindices.push_back(cyc);
+        pindices.push_back((unsigned int)symmDP);
+        
+        GET_FUNCTION_ADDR(ptr_to_FLATTE);
+        
+        initialize(pindices);
+    }
+    
+    Spline::Spline(std::string name,
+                               Variable *ar,
+                               Variable *ai,
+                               std::vector<fptype> &HH_bin_limits,
+                               std::vector<Variable *> &pwa_coefs_reals,
+                               std::vector<Variable *> &pwa_coefs_imags,
+                               unsigned int cyc,
+                               const bool symmDP)
+    : ResonancePdf(name, ar, ai) {
+        
+        std::vector<unsigned int> pindices;
+        const unsigned int nKnobs = HH_bin_limits.size();
+        host_constants.resize(nKnobs);
+        
+        pindices.push_back(0);
+        pindices.push_back(cyc);
+        pindices.push_back((unsigned int)symmDP);
+        pindices.push_back(nKnobs);
+        
+        for(int i = 0; i < pwa_coefs_reals.size(); i++) {
+            host_constants[i] = HH_bin_limits[i];
+            pindices.push_back(registerParameter(pwa_coefs_reals[i]));
+            pindices.push_back(registerParameter(pwa_coefs_imags[i]));
         }
-        std::vector<fptype> y2_flat = flatten(complex_derivative(host_constants, y));
-
-        MEMCPY_TO_SYMBOL(cDeriatives, y2_flat.data(), 2 * nKnobs * sizeof(fptype), 0, cudaMemcpyHostToDevice);
+        pindices.push_back(registerConstants(nKnobs));
+        
+        MEMCPY_TO_SYMBOL(functorConstants,
+                         host_constants.data(),
+                         nKnobs * sizeof(fptype),
+                         cIndex * sizeof(fptype),
+                         cudaMemcpyHostToDevice);
+        
+        GET_FUNCTION_ADDR(ptr_to_SPLINE);
+        
+        initialize(pindices);
     }
+
+    
+    
+    
+    
+    
+__host__ void Spline::recalculateCache() const {
+    auto params           = getParameters();
+    const unsigned nKnobs = params.size() / 2;
+    std::vector<fpcomplex> y(nKnobs);
+    unsigned int i = 0;
+    for(auto v = params.begin(); v != params.end(); ++v, ++i) {
+        unsigned int idx = i / 2;
+        fptype value     = host_params[(*v)->getIndex()];
+        if(i % 2 == 0)
+            y[idx].real(value);
+        else
+            y[idx].imag(value);
+    }
+    std::vector<fptype> y2_flat = flatten(complex_derivative(host_constants, y));
+    
+    MEMCPY_TO_SYMBOL(cDeriatives, y2_flat.data(), 2 * nKnobs * sizeof(fptype), 0, cudaMemcpyHostToDevice);
 }
+
+    
+} // namespace Resonances
 
 } // namespace GooFit

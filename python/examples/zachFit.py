003cb22d0262511fab4173a9f87cb8dfbb56053d
Himadri Pandey <shivahimadri@gmail.com>
2017-10-26 12:18:34 -0400

    updated examples and bindings

--- python/examples/zachFit.py
+++ python/examples/zachFit.py
@@ -2,163 +2,101 @@
 # -*- coding: utf-8 -*-
 
 def main():
-    mode = 0, data = 0;
-    plot;
+    mode = 0, data = 0
+    plot
 
-       # Get the name of the files to use
-    std::string mcfile, datafile;
-    if(data == 0) {
-        mcfile   = app.get_filename("dataFiles/dstwidth_kpi_resMC.dat", "examples/zachFit");
-        datafile = app.get_filename("dataFiles/dstwidth_kpi_data.dat", "examples/zachFit");
-    } else if(data == 1) {
-        mcfile   = app.get_filename("dataFiles/DstarWidth_D0ToKpi_deltaM_MC.dat", "examples/zachFit");
-        datafile = app.get_filename("dataFiles/DstarWidth_D0ToKpi_deltaM_Data.dat", "examples/zachFit");
-    } else {
-        mcfile   = app.get_filename("dataFiles/DstarWidth_D0ToK3pi_deltaM_MC.dat", "examples/zachFit");
-        datafile = app.get_filename("dataFiles/DstarWidth_D0ToK3pi_deltaM_Data.dat", "examples/zachFit");
-    }
+    dm = Variable("dm", 0.1395, 0.1665)
+    dm.setNumBins(2700)
 
-    Variable dm{"dm", 0.1395, 0.1665};
-    dm.setNumBins(2700);
+    mc_dataset = ()
+    data_dataset = ()
 
-    # This would be clearer with std::optional from C++17
-    std::unique_ptr<DataSet> mc_dataset, data_dataset;
+    if mode == 0:
+        mc_dataset.reset(UnbinnedDataSet(dm))
+        data_dataset.reset(UnbinnedDataSet(dm))
+    else:
+        mc_dataset.reset(BinnedDataSet(dm))
+        data_dataset.reset(BinnedDataSet(dm))
 
-    if(mode == 0) {
-        mc_dataset.reset(new UnbinnedDataSet{&dm});
-        data_dataset.reset(new UnbinnedDataSet{&dm});
-    } else {
-        mc_dataset.reset(new BinnedDataSet{&dm});
-        data_dataset.reset(new BinnedDataSet{&dm});
-    }
+    mean1 = Variable("kpi_mc_mean1", 0.145402, 0.00001, 0.143, 0.148)
+    mean2 = Variable("kpi_mc_mean2", 0.145465, 0.00001, 0.145, 0.1465)
+    mean3 = Variable("kpi_mc_mean3", 0.145404, 0.00001, 0.144, 0.147)
 
-    TH1D *mc_hist = getMCData(mc_dataset.get(), &dm, mcfile);
+    sigma1 = Variable("kpi_mc_sigma1", 0.00010, 0.00001, 0.000001, 0.002)
+    sigma2 = Variable("kpi_mc_sigma2", 0.00075, 0.00001, 0.000001, 0.005)
+    sigma3 = Variable("kpi_mc_sigma3", 0.00020, 0.00001, 0.000005, 0.001)
 
-    Variable mean1("kpi_mc_mean1", 0.145402, 0.00001, 0.143, 0.148);
-    Variable mean2("kpi_mc_mean2", 0.145465, 0.00001, 0.145, 0.1465);
-    Variable mean3("kpi_mc_mean3", 0.145404, 0.00001, 0.144, 0.147);
+    pimass = Variable("kpi_mc_pimass", 0.13957)
+    aslope = Variable("kpi_mc_aslope", -20.0, 1, -100.0, 10.0)
+    apower = Variable("kpi_mc_apower", 1.3, 0.1, 0.1, 10.0)
+    gfrac1 = Variable("kpi_mc_gfrac1", 0.65, 0.01, 0.0, 0.9)
+    gfrac2 = Variable("kpi_mc_gfrac2", 0.02, 0.001, 0.0, 0.12)
+    afrac = Variable("kpi_mc_afrac", 0.005, 0.003, 0.0, 0.1)
 
-    Variable sigma1("kpi_mc_sigma1", 0.00010, 0.00001, 0.000001, 0.002);
-    Variable sigma2("kpi_mc_sigma2", 0.00075, 0.00001, 0.000001, 0.005);
-    Variable sigma3("kpi_mc_sigma3", 0.00020, 0.00001, 0.000005, 0.001);
+    gauss1 = GaussianPdf("gauss1", dm, mean1, sigma1)
+    gauss2 = GaussianPdf("gauss2", dm, mean2, sigma2)
+    gauss3 = GaussianPdf("gauss3", dm, mean3, sigma3)
+    argus = ArgusPdf("argus", dm, pimass, aslope, false, apower)
 
-    Variable pimass("kpi_mc_pimass", 0.13957);
-    Variable aslope("kpi_mc_aslope", -20.0, 1, -100.0, 10.0);
-    Variable apower("kpi_mc_apower", 1.3, 0.1, 0.1, 10.0);
-    Variable gfrac1("kpi_mc_gfrac1", 0.65, 0.01, 0.0, 0.9);
-    Variable gfrac2("kpi_mc_gfrac2", 0.02, 0.001, 0.0, 0.12);
-    Variable afrac("kpi_mc_afrac", 0.005, 0.003, 0.0, 0.1);
+    resolution = AddPdf("resolution", (gfrac1, gfrac2, afrac), (gauss1, gauss2, argus, gauss3))
 
-    GaussianPdf gauss1("gauss1", &dm, &mean1, &sigma1);
-    GaussianPdf gauss2("gauss2", &dm, &mean2, &sigma2);
-    GaussianPdf gauss3("gauss3", &dm, &mean3, &sigma3);
-    ArgusPdf argus("argus", &dm, &pimass, &aslope, false, &apower);
+    resolution.setData(mc_dataset.get())
 
-    AddPdf resolution{"resolution", {&gfrac1, &gfrac2, &afrac}, {&gauss1, &gauss2, &argus, &gauss3}};
-
-    resolution.setData(mc_dataset.get());
-
-    FitManager mcpdf{&resolution};
-
-    GOOFIT_INFO("Done with collecting MC, starting minimisation");
-    mcpdf.fit();
-
-    if(plot) {
-        GOOFIT_INFO("Plotting MC");
-        mc_hist->SetLineColor(kBlack);
-        mc_hist->Draw("e");
-
-        double step   = mc_hist->GetXaxis()->GetBinWidth(2);
-        auto tot_hist = resolution.plotToROOT(&dm, mc_dataset->getNumEvents() * step);
-        tot_hist->SetLineColor(kGreen);
-
-        tot_hist->Draw("SAME");
-
-        foo.SaveAs("MC_plot.png");
-    }
+    mcpdf = FitManagr(resolution)
+    mcpdf.fit()
 
     # Locking the MC variables
-    mean1.setFixed(true);
-    mean2.setFixed(true);
-    mean3.setFixed(true);
-    sigma1.setFixed(true);
-    sigma2.setFixed(true);
-    sigma3.setFixed(true);
-    pimass.setFixed(true);
-    aslope.setFixed(true);
-    gfrac1.setFixed(true);
-    gfrac2.setFixed(true);
-    afrac.setFixed(true);
-    apower.setFixed(true);
+    mean1.setFixed(True)
+    mean2.setFixed(True)
+    mean3.setFixed(True)
+    sigma1.setFixed(True)
+    sigma2.setFixed(True)
+    sigma3.setFixed(True)
+    pimass.setFixed(True)
+    aslope.setFixed(True)
+    gfrac1.setFixed(True)
+    gfrac2.setFixed(True)
+    afrac.setFixed(True)
+    apower.setFixed(True)
 
-    Variable dummyzero("kpi_rd_dummyzero", 0);
-    Variable delta("kpi_rd_delta", 0.000002, -0.00005, 0.00005);
-    Variable epsilon("kpi_rd_epsilon", 0.05, -0.1, 0.2);
+    dummyzero = Variable("kpi_rd_dummyzero", 0)
+    delta = Variable("kpi_rd_delta", 0.000002, -0.00005, 0.00005)
+    epsilon = Variable("kpi_rd_epsilon", 0.05, -0.1, 0.2)
 
-    ScaledGaussianPdf resolution1("resolution1", &dm, &dummyzero, &sigma1, &delta, &epsilon);
-    ScaledGaussianPdf resolution2("resolution2", &dm, &dummyzero, &sigma2, &delta, &epsilon);
-    ScaledGaussianPdf resolution3("resolution3", &dm, &dummyzero, &sigma3, &delta, &epsilon);
+    resolution1 = ScaledGaussianPdf("resolution1", dm, dummyzero, sigma1, delta, epsilon)
+    resolution2 = ScaledGaussianPdf("resolution2", dm, dummyzero, sigma2, delta, epsilon)
+    resolution3 = ScaledGaussianPdf("resolution3", dm, dummyzero, sigma3, delta, epsilon)
 
-    Variable width_bw("kpi_rd_width_bw", 0.0001, 0.00001, 0.0005);
-    KinLimitBWPdf rbw1("rbw1", &dm, &mean1, &width_bw);
-    KinLimitBWPdf rbw2("rbw2", &dm, &mean2, &width_bw);
-    KinLimitBWPdf rbw3("rbw3", &dm, &mean3, &width_bw);
+    width_bw = Variable("kpi_rd_width_bw", 0.0001, 0.00001, 0.0005)
+    rbw1 = KinLimitBWPdf("rbw1", dm, mean1, width_bw)
+    rbw2 = KinLimitBWPdf("rbw2", dm, mean2, width_bw)
+    rbw3 = KinLimitBWPdf("rbw3", dm, mean3, width_bw)
 
-    ConvolutionPdf signal1{"signal1", &dm, &rbw1, &resolution1};
-    ConvolutionPdf signal2{"signal2", &dm, &rbw2, &resolution2};
-    ConvolutionPdf signal3{"signal3", &dm, &rbw3, &resolution3};
+    signal1 = ConvolutionPdf("signal1", dm, rbw1, resolution1)
+    signal2 = ConvolutionPdf("signal2", dm, rbw2, resolution2)
+    signal3 = ConvolutionPdf("signal3", dm, rbw3, resolution3)
 
-    signal1.setIntegrationConstants(0.1395, 0.1665, 0.0000027);
-    signal2.setIntegrationConstants(0.1395, 0.1665, 0.0000027);
-    signal3.setIntegrationConstants(0.1395, 0.1665, 0.0000027);
+    signal1.setIntegrationConstants(0.1395, 0.1665, 0.0000027)
+    signal2.setIntegrationConstants(0.1395, 0.1665, 0.0000027)
+    signal3.setIntegrationConstants(0.1395, 0.1665, 0.0000027)
 
-    AddPdf signal{"signal", {&gfrac1, &gfrac2, &afrac}, {&signal1, &signal2, &argus, &signal3}};
+    signal = AddPdf("signal", (gfrac1, gfrac2, afrac), (signal1, signal2, argus, signal3))
 
-    Variable slope("kpi_rd_slope", -1.0, 0.1, -35.0, 25.0);
-    Variable *bpower = nullptr;
-    ArgusPdf bkg("bkg", &dm, &pimass, &slope, false, bpower);
+    slope = Variable("kpi_rd_slope", -1.0, 0.1, -35.0, 25.0)
+    bpower = nullptr
+    bkg = ArgusPdf("bkg", dm, pimass, slope, false, bpower)
+    bkg_frac = Variable("kpi_rd_bkg_frac", 0.03, 0.0, 0.3)
+    total = AddPdf("total", (bkg_frac), (bkg, signal))
+    total.setData(data_dataset.get())
 
-    Variable bkg_frac("kpi_rd_bkg_frac", 0.03, 0.0, 0.3);
+    chi_control = ()
+    if(2 == mode) (
+        chi_control.reset(BinnedChisqFit)
+        total.setFitControl(chi_control.get())
+    )
 
-    TH1D *data_hist = getData(data_dataset.get(), &dm, datafile);
-
-    AddPdf total("total", {&bkg_frac}, {&bkg, &signal});
-
-    total.setData(data_dataset.get());
-
-    std::unique_ptr<BinnedChisqFit> chi_control;
-    if(2 == mode) {
-        chi_control.reset(new BinnedChisqFit);
-        total.setFitControl(chi_control.get());
-    }
-
-    FitManager datapdf{&total};
-
-    GOOFIT_INFO("Starting fit");
-
-    datapdf.fit();
-
-    if(plot) {
-        GOOFIT_INFO("Plotting results");
-
-        data_hist->SetLineColor(kBlack);
-        data_hist->Draw("e");
-
-        double scale = data_hist->GetXaxis()->GetBinWidth(2) * data_dataset->getNumEvents();
-
-        auto sig_hist = signal.plotToROOT(&dm, (1 - bkg_frac.getValue()) * scale);
-        sig_hist->SetLineColor(kBlue);
-        auto back_hist = bkg.plotToROOT(&dm, bkg_frac.getValue() * scale);
-        back_hist->SetLineColor(kRed);
-        auto tot_hist = total.plotToROOT(&dm, scale);
-        tot_hist->SetLineColor(kGreen);
-
-        tot_hist->Draw("SAME");
-        sig_hist->Draw("SAME");
-        back_hist->Draw("SAME");
-
-        foo.SaveAs("ResultFit.png");
-    }
-
-    return datapdf;
-}
+    FitManager datapdf(total)
+    datapdf.fit()
+    return datapdf
+)
+main()

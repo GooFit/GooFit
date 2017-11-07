3428c23864c6e34e1df5c419506c74f8ac3b58a1
Himadri Pandey <shivahimadri@gmail.com>
2017-11-06 22:08:48 -0500

    Added labels to ArgusPdf

--- python/PDFs/basic/ArgusPdf.cu
+++ python/PDFs/basic/ArgusPdf.cu
@@ -9,10 +9,21 @@ namespace py = pybind11;
 void init_ArgusPdf(py::module &m) {
     py::class_<ArgusPdf, GooPdf>(m, "ArgusPdf")
         .def(py::init<std::string, Variable *, Variable *, Variable *, bool>(),
+             "n",
+             "_x",
+             "m",
+             "s",
+             "upper",
              py::keep_alive<1, 3>(),
              py::keep_alive<1, 4>(),
              py::keep_alive<1, 5>())
         .def(py::init<std::string, Variable *, Variable *, Variable *, bool, Variable *>(),
+             "n",
+             "_x",
+             "m",
+             "s",
+             "upper",
+             "power",
              py::keep_alive<1, 3>(),
              py::keep_alive<1, 4>(),
              py::keep_alive<1, 5>());

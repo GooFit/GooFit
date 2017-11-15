d76f3e50648c93704990612222eaaf48665dc3b3
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-11-15 14:02:13 -0500

    Fixing holders, adding rest of the Fit types

--- python/goofit/FitControl.cpp
+++ python/goofit/FitControl.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 #include <pybind11/iostream.h>
 #include <pybind11/pybind11.h>
@@ -13,12 +14,14 @@ using namespace GooFit;
 using namespace pybind11::literals;
 
 void init_FitControl(py::module &m) {
-    py::class_<FitControl>(m, "FitControl")
+    py::class_<FitControl, std::shared_ptr<FitControl>>(m, "FitControl")
         .def("binnedFit", &FitControl::binnedFit)
         .def("binErrors", &FitControl::binErrors)
         .def("metricIsPdf", &FitControl::metricIsPdf)
         .def("getMetric", &FitControl::getMetric);
-    //.def_property("owner", &FitControl::getOwner, &FitControl::setOwner);
 
-    py::class_<BinnedErrorFit, FitControl>(m, "BinnedErrorFit").def(py::init<>());
+    py::class_<BinnedErrorFit, FitControl, std::shared_ptr<BinnedErrorFit>>(m, "BinnedErrorFit").def(py::init<>());
+    py::class_<UnbinnedNllFit, FitControl, std::shared_ptr<UnbinnedNllFit>>(m, "UnbinnedNllFit").def(py::init<>());
+    py::class_<BinnedNllFit, FitControl, std::shared_ptr<BinnedNllFit>>(m, "BinnedNllFit").def(py::init<>());
+    py::class_<BinnedChisqFit, FitControl, std::shared_ptr<BinnedChisqFit>>(m, "BinnedChisqFit").def(py::init<>());
 }

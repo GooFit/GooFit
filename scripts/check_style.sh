dccd890c2fe20a828f92eabc53c1db889ab02c63
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-11-17 13:02:43 -0500

    Dropping .cc as a goofit extension in source

--- scripts/check_style.sh
+++ scripts/check_style.sh
@@ -3,7 +3,7 @@ set -evx
 
 clang-format --version
 
-git ls-files -- '*.cpp' '*.hpp' '*.cu' '*.cc' '*.h' | xargs clang-format -style=file -sort-includes -i
+git ls-files -- '*.cpp' '*.hpp' '*.cu' '*.h' | xargs clang-format -style=file -sort-includes -i
 
 git diff --exit-code --color
 

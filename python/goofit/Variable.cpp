7a4fd7eb64246c023acc48840df4529686a28588
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-12-07 14:40:05 -0500

    More ways to access variables, to mimic C++ goofit

--- python/goofit/Variable.cpp
+++ python/goofit/Variable.cpp
@@ -1,6 +1,8 @@
 #include <goofit/Variable.h>
 #include <pybind11/pybind11.h>
 
+#include "props.h"
+
 #include <fmt/format.h>
 #include <sstream>
 #include <string>
@@ -12,16 +14,16 @@ using namespace GooFit;
 void init_Variable(py::module &m) {
     py::class_<Indexable>(m, "Indexable")
         .def_property_readonly("name", &Indexable::getName)
-        .def_property("value", &Indexable::getValue, &Indexable::setValue)
-        .def_property("index", &Indexable::getIndex, &Indexable::setIndex)
-        .def_property("upperlimit", &Indexable::getUpperLimit, &Indexable::setUpperLimit)
-        .def_property("lowerlimit", &Indexable::getLowerLimit, &Indexable::setLowerLimit)
+        ADD_PROP(value, getValue, setValue, Indexable)
+        ADD_PROP(index, getIndex, setIndex, Indexable)
+        ADD_PROP(upperlimit, getUpperLimit, setUpperLimit, Indexable)
+        ADD_PROP(lowerlimit, getLowerLimit, setLowerLimit, Indexable)
         .def("__repr__", [](const Indexable &v) { return "<Indexable: {}>"_format(v.getName()); })
         .def("__bool__", &Indexable::operator bool);
 
     py::class_<Observable, Indexable>(m, "Observable")
         .def(py::init<std::string, fptype, fptype>()) // , "name"_a, "min"_a, "max"_a)
-        .def_property("numbins", &Observable::getNumBins, &Observable::setNumBins)
+        ADD_PROP(numbins, getNumBins, setNumBins, Observable)
         .def("__repr__", [](const Observable &v) { return "<Observable: {}>"_format(v.getName()); })
         .def("__str__", [](const Observable &v) {
             std::stringstream os;
@@ -35,10 +37,10 @@ void init_Variable(py::module &m) {
         .def(py::init<std::string, fptype, fptype, fptype, fptype>())
         .def(py::init<std::string, fptype, fptype, fptype, fptype, bool>())
         // "name"_a, "value"_a, "error"_a, "min"_a, "max"_a, "fixed"_a=false)
-        .def_property("error", &Variable::getError, &Variable::setError)
-        .def_property("fixed", &Variable::IsFixed, &Variable::setFixed)
-        .def_property_readonly("fitterIndex", &Variable::getFitterIndex)
-        .def("setBlind", &Variable::setBlind)
+        ADD_PROP(error, getError, setError, Variable)
+        ADD_PROP(fixed, IsFixed, setFixed, Variable)
+        ADD_PROP_RO(fitterIndex, getFitterIndex, Variable)
+        ADD_PROP_WO(blind, setBlind, Variable)
         .def("__repr__", [](const Variable &v) { return "<Variable: {}>"_format(v.getName()); })
         .def("__str__", [](const Variable &v) {
             std::stringstream os;

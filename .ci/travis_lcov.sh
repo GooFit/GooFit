831eabbfc053f14411049af63f528328598f2fe0
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-02-16 13:34:03 +0100

    Changing code coverage in Travis

---
+++ .ci/travis_lcov.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -evx
+
+cd ${TRAVIS_BUILD_DIR}
+
+mkdir build || true
+cd build
+cmake -DGOOFIT_DEVICE=OMP -DGOOFIT_TESTS=ON -DCMAKE_BUILD_TYPE=Coverage -DGOOFIT_PYTHON=OFF ..
+cmake --build . -- -j2
+cmake --build . --target GooFit_coverage
+
+lcov --directory . --capture --output-file coverage.info # capture coverage info
+lcov --remove coverage.info '*/tests/*' '*gtest*' '*gmock*' '/usr/*' --output-file coverage.info # filter out system
+lcov --list coverage.info #debug info
+# Uploading report to CodeCov
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
+

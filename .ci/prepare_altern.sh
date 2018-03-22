500e3ed54417e87c0a595d673cc2dbb8aef0e6df
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-03-22 14:57:50 +0100

    Faster travis after most recent update

--- .ci/prepare_altern.sh
+++ .ci/prepare_altern.sh
@@ -7,12 +7,6 @@ if [ "$CXX" = "g++" ] ; then
     ln -s `which gcc-$COMPILER` gcc
     ln -s `which g++-$COMPILER` g++
     ln -s `which gcov-$COMPILER` gcov
-else
-    ln -s `which clang-$COMPILER` clang
-    ln -s `which clang++-$COMPILER` clang++
-    ln -s `which clang-format-$COMPILER` clang-format
-    ln -s `which clang-tidy-$COMPILER` clang-tidy
-    export CXXFLAGS="-stdlib=libc++"
 fi
 
 export PATH="${DEPS_DIR}/extrabin":$PATH

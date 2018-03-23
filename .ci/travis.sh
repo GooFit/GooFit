1c312e533b00b4059b68df3d12bc78a0c532945c
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-03-23 22:01:53 +0100

    Access to a few more items, drop forced Poisson fluctuation of event number

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -11,7 +11,7 @@ cmake -DGOOFIT_DEVICE=OMP -DCMAKE_BUILD_TYPE=RelWithDebInfo -DGOOFIT_PYTHON=ON -
 cmake --build . -- -j2
 
 set +evx
-echo -en 'travis_fold:end:script.build\\r'
+echo -e 'travis_fold:end:script.build\\r'
 echo -en 'travis_fold:start:script.test\\r'
 echo "Testing..."
 set -evx
@@ -21,7 +21,7 @@ ctest --output-on-failure
 
 
 set +evx
-echo -en 'travis_fold:end:script.test\\r'
+echo -e 'travis_fold:end:script.test\\r'
 echo -en 'travis_fold:start:script.examples\\r'
 echo "Python testing..."
 set -evx
@@ -29,7 +29,7 @@ set -evx
 ./examples/RunAll.py
 
 set +evx
-echo -en 'travis_fold:end:script.examples\\r'
+echo -e 'travis_fold:end:script.examples\\r'
 echo -en 'travis_fold:start:script.pytest\\r'
 echo "Python testing..."
 set -evx
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`
 pytest
 
 set +evx
-echo -en 'travis_fold:end:script.pytest\\r'
+echo -e 'travis_fold:end:script.pytest\\r'
 echo -en 'travis_fold:start:script.pyexamples\\r'
 echo "Python examples..."
 set -evx
@@ -46,4 +46,4 @@ set -evx
 ./pyexamples/RunAll.sh
 
 set +evx
-echo -en 'travis_fold:end:script.pyexamples\\r'
+echo -e 'travis_fold:end:script.pyexamples\\r'

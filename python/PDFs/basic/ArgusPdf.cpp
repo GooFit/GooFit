7ec0ed6443d03efe32988b88430680ebbcb62bc8
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-05-25 11:48:24 +0200

    Better printing

--- python/PDFs/basic/ArgusPdf.cpp
+++ python/PDFs/basic/ArgusPdf.cpp
@@ -7,7 +7,21 @@ using namespace GooFit;
 namespace py = pybind11;
 using namespace pybind11::literals;
 
+struct HelpPrinter {
+    std::string help_str;
+    std::string getHelp() const { return help_str; }
+    HelpPrinter(std::string input)
+        : help_str(input) {}
+};
+
 void init_ArgusPdf(py::module &m) {
+    py::class_<HelpPrinter>(m, "HelpPrinter")
+        .def(py::init<std::string>())
+        .def("_repr_markdown_", &HelpPrinter::getHelp)
+        .def("__repr__", &HelpPrinter::getHelp);
+
+    m.attr("ArgusHelp") = HelpPrinter(ArgusHelp);
+
     py::class_<ArgusPdf, GooPdf>(m, "ArgusPdf")
         .def(py::init<std::string, Observable, Variable, Variable, bool>(),
              "Standard Argus",
@@ -24,5 +38,6 @@ void init_ArgusPdf(py::module &m) {
              "m"_a,
              "s"_a,
              "upper"_a,
-             "power"_a);
+             "power"_a)
+        .def_static("help", []() { return HelpPrinter(ArgusHelp); });
 }

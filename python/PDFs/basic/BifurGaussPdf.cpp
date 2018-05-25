b031bdab00ec3d746d8f69f740fbf6cbadeb5669
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-05-25 23:44:33 +0200

    Adding BiGauss

--- python/PDFs/basic/BifurGaussPdf.cpp
+++ python/PDFs/basic/BifurGaussPdf.cpp
@@ -1,12 +1,20 @@
-#include <pybind11/pybind11.h>
+#include <goofit/Python.h>
 
 #include <goofit/PDFs/basic/BifurGaussPdf.h>
 #include <goofit/Variable.h>
+#include <goofit/docs/PDFs/basic/BifurGaussPdf.h>
 
 using namespace GooFit;
-namespace py = pybind11;
 
 void init_BifurGaussPdf(py::module &m) {
     py::class_<BifurGaussPdf, GooPdf>(m, "BifurGaussPdf")
-        .def(py::init<std::string, Observable, Variable, Variable, Variable>(), "n", "_x", "m", "sL", "sR");
+        .def(py::init<std::string, Observable, Variable, Variable, Variable>(),
+             "A two-sided Gaussian, with a sigma that varies "
+             "depending on which side of the mean you are on",
+             "n"_a,
+             "x"_a,
+             "m"_a,
+             "sL"_a,
+             "sR"_a)
+        .def_static("help", []() { return HelpPrinter(BifurGaussPdf_docs); });
 }

fa3bed4e26427347de1a576148107c20beea2b0f
Henry Schreiner <HenrySchreinerIII@gmail.com>
2018-05-26 23:17:15 +0200

    Adding CrystalBall and fixes

--- include/goofit/PDFs/basic/CrystalBallPdf.h
+++ include/goofit/PDFs/basic/CrystalBallPdf.h
@@ -4,6 +4,25 @@
 
 namespace GooFit {
 
+/**
+A Gaussian with a power-law tail on one side:
+
+\f[
+    P(x;m,\sigma,\alpha,p) &=& \left\{ \begin{matrix}
+    e^{-\frac{(x-m)^2}{2\sigma^2}} & \mathrm{sg}(\alpha)\frac{x - m}{\sigma} \le \mathrm{sg}(\alpha)\alpha \\
+    e^{-\alpha^2/2}\left(\frac{p/\alpha}{p/\alpha - \alpha + \frac{x-m}{\sigma}}\right)^p
+    & \mathrm{otherwise } (\alpha\ne 0). \\
+    \end{matrix}
+    \right.
+\f]
+
+The constructor takes the observable \f$x\f$,
+the mean \f$m\f$, width \f$\sigma\f$, cutoff \f$\alpha\f$, and power \f$p\f$. Note
+that if \f$\alpha\f$ is negative, the power-law tail is on the right; if
+positive, on the left. For \f$\alpha=0\f$, the function reduces to a
+simple Gaussian in order to avoid \f$p/\alpha\f$ blowing up.
+**/
+
 class CrystalBallPdf : public GooPdf {
   public:
     CrystalBallPdf(std::string n, Observable _x, Variable m, Variable s, Variable a);

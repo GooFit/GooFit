cef48aef8a47a73088681cce0a377a6c909cd60a
Henry Schreiner <HenrySchreinerIII@gmail.com>
2018-05-26 23:40:10 +0200

    Adding exp docs

--- include/goofit/PDFs/basic/ExpPdf.h
+++ include/goofit/PDFs/basic/ExpPdf.h
@@ -4,6 +4,28 @@
 
 namespace GooFit {
 
+/**
+A plain exponential:
+
+\f[
+    P(x;\alpha, x_0) = e^{\alpha(x-x_0)}
+\f]
+
+taking the
+observable \f$x\f$, exponential constant \f$\alpha\f$, and optional offset
+\f$x_0\f$. If \f$x_0\f$ is not specified it defaults to zero.
+
+A variant constructor takes, in place of \f$\alpha\f$, a `vector` of coefficients
+(in the order \f$\alpha_0\f$ to \f$\alpha_n\f$) to form a polynomial in the exponent:
+
+\f[
+P(x;\alpha_0, \alpha_1, \ldots \alpha_n, x_0) = e^{\alpha_0 + \alpha_1(x-x_0) + \alpha_2(x-x_0)^2 + \ldots + \alpha_n(x-x_0)^n}
+\f]
+
+The offset \f$x_0\f$ is again optional and defaults to zero.
+**/
+
+
 class ExpPdf : public GooPdf {
   public:
     ExpPdf(std::string n, Observable _x, Variable alpha);
--- python/PDFs/basic/ExpPdf.cpp
+++ python/PDFs/basic/ExpPdf.cpp
@@ -1,13 +1,18 @@
-#include <pybind11/pybind11.h>
+#include <goofit/Python.h>
 
 #include <goofit/PDFs/basic/ExpPdf.h>
+#include <goofit/docs/PDFs/basic/ExpPdf.h>
 #include <goofit/Variable.h>
 
 using namespace GooFit;
-namespace py = pybind11;
 
 void init_ExpPdf(py::module &m) {
     py::class_<ExpPdf, GooPdf>(m, "ExpPdf")
-        .def(py::init<std::string, Observable, Variable>(), "n", "_x", "alpha")
-        .def(py::init<std::string, Observable, Variable, Variable>(), "n", "_x", "alpha", "offset");
+        .def(py::init<std::string, Observable, Variable>(), "A plain exponential.", "name"_a, "x"_a, "alpha"_a)
+        
+        .def(py::init<std::string, Observable, Variable, Variable>(), "A plain exponential.", "name"_a, "x"_a, "alpha"_a, "offset"_a)
+
+        .def_static("help", []() { return HelpPrinter(ExpPdf_docs); });
+
+// TODO: add stl and missing constructors
 }

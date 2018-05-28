a314ac0e73c231ae58859e6ddfa881b34becf1aa
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-05-28 14:27:36 +0200

    Trying to fix symbol issue on Python

--- python/goofit/Application.cpp
+++ python/goofit/Application.cpp
@@ -10,11 +10,12 @@ using namespace GooFit;
 
 void init_Application(py::module &m) {
     m.def("print_splash", &print_splash, "Print a splash screen", py::call_guard<py::scoped_ostream_redirect>());
+
     m.def("print_goofit_info",
           &print_goofit_info,
           "Print GooFit information",
           "gpuDevice"_a = 0,
-          py::call_guard<py::scoped_ostream_redirect>())
+          py::call_guard<py::scoped_ostream_redirect>());
 
-        ;
+    m.def("__main__", []() { print_goofit_info(0); }, py::call_guard<py::scoped_ostream_redirect>());
 }

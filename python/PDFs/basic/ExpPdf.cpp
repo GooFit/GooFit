510c6fc0e8f811ab580b4f20ecedcbf00f1c3447
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-05-28 16:59:37 +0200

    Adding extra constructors

--- python/PDFs/basic/ExpPdf.cpp
+++ python/PDFs/basic/ExpPdf.cpp
@@ -12,14 +12,27 @@ void init_ExpPdf(py::module &m) {
     py::class_<ExpPdf, GooPdf>(m, "ExpPdf")
         .def(py::init<std::string, Observable, Variable>(), "A plain exponential.", "name"_a, "x"_a, "alpha"_a)
 
+        .def(py::init<std::string, Observable, std::vector<Variable> &>(),
+             "An exponential raised to a polynomial.",
+             "name"_a,
+             "x"_a,
+             "coefs"_a)
+
         .def(py::init<std::string, Observable, Variable, Variable>(),
-             "A plain exponential.",
+             "A plain exponential with offset.",
              "name"_a,
              "x"_a,
              "alpha"_a,
              "offset"_a)
 
-        .def_static("help", []() { return HelpPrinter(ExpPdf_docs); });
+        .def(py::init<std::string, Observable, std::vector<Variable> &, Variable>(),
+             "An exponential raised to a polynomial with offset.",
+             "name"_a,
+             "x"_a,
+             "coefs"_a,
+             "offset"_a)
 
-    // TODO: add stl and missing constructors
+        .def_static("help", []() { return HelpPrinter(ExpPdf_docs); })
+
+        ;
 }

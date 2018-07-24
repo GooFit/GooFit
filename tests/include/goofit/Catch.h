f117023bc883971778956adcd2ab2625844fc245
Henry Schreiner <hschrein@cern.ch>
2018-07-24 05:53:10 -0400

    Updating Catch2, using Approx

--- tests/include/goofit/Catch.h
+++ tests/include/goofit/Catch.h
@@ -1,14 +1,11 @@
 #pragma once
 
-#include <catch.hpp>
+#include <catch2/catch.hpp>
 
 #include <goofit/PDFs/GooPdf.h>
 #include <goofit/Variable.h>
 
 using namespace GooFit;
+using namespace Catch::literals;
 
 std::string capture_stdout(std::function<void()> &&func);
-
-inline Approx operator"" _a(long double val) { return Approx(val); }
-
-inline Approx operator"" _a(unsigned long long val) { return Approx(val); }

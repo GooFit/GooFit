33c89d9c72332a613878da77fa7737dec07936a6
Henry Schreiner <hschrein@cern.ch>
2018-08-13 10:53:41 -0400

    Use docker to check style if clang-format is not installed and docker is
    [skip ci]

--- scripts/check_style.sh
+++ scripts/check_style.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env sh
-set -evx
 
-clang-format --version
+set -e
 
-git ls-files -- '*.cpp' '*.hpp' '*.cu' '*.h' | xargs clang-format -style=file -sort-includes -i
+if [ -x "$(command -v clang-format)" ] ; then
+
+    clang-format --version
+    git ls-files -- '*.cpp' '*.hpp' '*.cu' '*.h' | xargs clang-format -style=file -sort-includes -i
+
+elif [ -x "$(command -v docker)" ] ; then
+
+    docker run -it unibeautify/clang-format --version
+    docker run -it -v "$(pwd)":/workdir -w /workdir unibeautify/clang-format -style=file -sort-includes -i $(git ls-files -- '*.cpp' '*.hpp' '*.cu' '*.h')
+
+fi
 
 git diff --exit-code --color
 
-set +evx
+set +e
+

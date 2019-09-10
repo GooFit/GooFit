52dc56ead54c6aa4ff15d6ed6e4dbed22f9d43df
martha <martha@hepgpu5.blackett.manchester.ac.uk>
2019-09-10 11:21:05 +0100

    Remove docs.

--- python/PDFs/physics/SquareDalitzEffPdf.cpp
+++ python/PDFs/physics/SquareDalitzEffPdf.cpp
@@ -1,5 +1,8 @@
 #include <goofit/Python.h>
 
+#include <pybind11/complex.h>
+#include <pybind11/stl.h>
+
 #include <goofit/PDFs/physics/SquareDalitzEffPdf.h>
 
 using namespace GooFit;
@@ -7,7 +10,6 @@ using namespace GooFit;
 void init_SquareDalitzEffPdf(py::module &m) {
   py::class_<SquareDalitzEffPdf, GooPdf>(m, "SquareDalitzEffPdf")
     .def(py::init<std::string, Observable, Observable, Variable, Variable, Variable, Variable, Variable, Variable, Variable, Variable>(),
-	 SquareDalitzPdf_docs.c_str(),
 	 "name"_a,
 	 "m12"_a,
 	 "m13"_a,
@@ -19,6 +21,5 @@ void init_SquareDalitzEffPdf(py::module &m) {
 	 "c5"_a,
 	 "c6"_a,
 	 "c7"_a)
-    .def_static("help", []() { return HelpPrinter(SquareDalitzEffPdf_docs); });
-
+    ;
 }

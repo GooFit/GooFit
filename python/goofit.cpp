b2c670c81906d4807997bf4286ba58db551fb558
martha <martha@hepgpu5.blackett.manchester.ac.uk>
2019-09-10 11:21:22 +0100

    Add SquareDalitzEffPdf.

--- python/goofit.cpp
+++ python/goofit.cpp
@@ -65,6 +65,7 @@ void init_ResonancePdf(py::module &);
 void init_SpinFactors(py::module &);
 void init_ThreeGaussResolution(py::module &);
 void init_TruthResolution(py::module &);
+void init_SquareDalitzEffPdf(py::module &);
 
 // Utilities
 void init_VariableBinTransform1DPdf(py::module &);
@@ -135,6 +136,7 @@ PYBIND11_MODULE(_goofit, m) {
     init_SpinFactors(m);
     init_ThreeGaussResolution(m);
     init_TruthResolution(m);
+    init_SquareDalitzEffPdf(m);
 
     // Utilities
     init_VariableBinTransform1DPdf(m);

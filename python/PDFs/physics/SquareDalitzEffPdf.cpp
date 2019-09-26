e97f7a48695203d58b6e12deeb4c7527150d46c1
Henry Schreiner <hschrein@cern.ch>
2019-09-26 15:16:59 -0400

    Fix formatting

--- python/PDFs/physics/SquareDalitzEffPdf.cpp
+++ python/PDFs/physics/SquareDalitzEffPdf.cpp
@@ -8,18 +8,27 @@
 using namespace GooFit;
 
 void init_SquareDalitzEffPdf(py::module &m) {
-  py::class_<SquareDalitzEffPdf, GooPdf>(m, "SquareDalitzEffPdf")
-    .def(py::init<std::string,Observable, Observable, Variable, Variable, Variable, Variable, Variable, Variable, Variable, Variable>(),
-	 "name"_a,
-	 "m12"_a,
-	 "m13"_a,
-	 "c0"_a,
-	 "c1"_a,
-	 "c2"_a,
-	 "c3"_a,
-	 "c4"_a,
-	 "c5"_a,
-	 "c6"_a,
-	 "c7"_a)
-    ;
+    py::class_<SquareDalitzEffPdf, GooPdf>(m, "SquareDalitzEffPdf")
+        .def(py::init<std::string,
+                      Observable,
+                      Observable,
+                      Variable,
+                      Variable,
+                      Variable,
+                      Variable,
+                      Variable,
+                      Variable,
+                      Variable,
+                      Variable>(),
+             "name"_a,
+             "m12"_a,
+             "m13"_a,
+             "c0"_a,
+             "c1"_a,
+             "c2"_a,
+             "c3"_a,
+             "c4"_a,
+             "c5"_a,
+             "c6"_a,
+             "c7"_a);
 }

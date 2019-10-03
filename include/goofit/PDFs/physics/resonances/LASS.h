0c32bc180d1032eb5217f11d6ac5f5df536a856d
martha <martha@hepgpu5.blackett.manchester.ac.uk>
2019-10-03 09:43:38 +0100

    Make LASS parameters free instead of fixed.

--- include/goofit/PDFs/physics/resonances/LASS.h
+++ include/goofit/PDFs/physics/resonances/LASS.h
@@ -9,7 +9,19 @@ namespace Resonances {
 /// LASS
 class LASS : public ResonancePdf {
   public:
-    LASS(std::string name, Variable ar, Variable ai, Variable mass, Variable width, unsigned int sp, unsigned int cyc);
+    LASS(std::string name, 
+         Variable ar, 
+         Variable ai, 
+         Variable mass, 
+         Variable width,
+         Variable _a,       
+         Variable _r,
+         Variable _R,
+         Variable _phiR,
+         Variable _B,
+         Variable _phiB,      
+         unsigned int sp, 
+         unsigned int cyc);
     ~LASS() override = default;
 };
 

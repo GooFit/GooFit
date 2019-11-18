e750b52b6c028f42a94e4b27aa0ec35ed471721d
Daniel Vieira <danielsibemol@gmail.com>
2019-11-18 17:11:34 -0500

    Fixing isnan on SquareDalitzEffPdf

--- src/PDFs/physics/SquareDalitzEffPdf.cu
+++ src/PDFs/physics/SquareDalitzEffPdf.cu
@@ -1,7 +1,6 @@
 #include <goofit/PDFs/ParameterContainer.h>
 #include <goofit/PDFs/physics/DalitzPlotHelpers.h>
 #include <goofit/PDFs/physics/SquareDalitzEffPdf.h>
-#include <cmath>
 #include <vector>
 
 namespace GooFit {
@@ -15,8 +14,7 @@ __device__ fptype thetaprime(fptype m12, fptype m13, fptype mD, fptype mKS0, fpt
     fptype num   = m23 * (m12 - m13);
     fptype denum = sqrt(((m23 - mh1 * mh1 + mh2 * mh2) * (m23 - mh1 * mh1 + mh2 * mh2) - 4 * m23 * mh2 * mh2))
                    * sqrt(((mD * mD - mKS0 * mKS0 - m23) * (mD * mD - mKS0 * mKS0 - m23) - 4 * m23 * mKS0 * mKS0));
-
-    if(isnan(denum)) {
+    if( isnan(denum)) {
         GOOFIT_TRACE("WARNING NAN.");
         return -99;
     }

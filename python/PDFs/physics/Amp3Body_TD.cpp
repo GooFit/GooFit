1a3f43234019db07dd529ab9bf063dac21ae693b
martha <martha@hepgpu4.blackett.manchester.ac.uk>
2020-05-08 15:57:06 +0100

    Add getDalitzHist to python bindings.

--- python/PDFs/physics/Amp3Body_TD.cpp
+++ python/PDFs/physics/Amp3Body_TD.cpp
@@ -118,6 +118,7 @@ void init_Amp3Body_TD(py::module &m) {
         .def("setDataSize", &Amp3Body_TD::setDataSize, "dataSize"_a, "evtSize"_a = 5)
         .def("getD0Fraction", &Amp3Body_TD::getD0Fraction)
         .def("setD0Fraction", &Amp3Body_TD::setD0Fraction, "d0fraction"_a)
+        .def("getDalitzHist", &Amp3Body_TD::getDalitzHist, "hist"_a)
 
         .def_static("help", []() { return HelpPrinter(Amp3Body_TD_docs); })
 

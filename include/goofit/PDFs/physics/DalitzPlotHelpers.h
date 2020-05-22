3e950e354fa12d5395756722b69aa97870425729
martha <martha@hepgpu4.blackett.manchester.ac.uk>
2020-05-22 15:30:55 +0100

    Change to host device function.

--- include/goofit/PDFs/physics/DalitzPlotHelpers.h
+++ include/goofit/PDFs/physics/DalitzPlotHelpers.h
@@ -26,7 +26,7 @@ constexpr typename std::underlying_type<E>::type enum_to_underlying(E e) {
     return static_cast<typename std::underlying_type<E>::type>(e);
 }
 
-__host__ int getDalitzBin(const fptype &m12, const fptype &m13);
+__host__ __device__ int getDalitzBin(const fptype &m12, const fptype &m13);
 
 __host__ __device__ bool inDalitz(
     const fptype &m12, const fptype &m13, const fptype &bigM, const fptype &dm1, const fptype &dm2, const fptype &dm3);

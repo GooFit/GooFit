e98e082c9e33443d99746663c50bfded2e4878e2
martha <martha@hepgpu4.blackett.manchester.ac.uk>
2020-07-24 11:22:01 +0100

    Remove comments.

--- include/goofit/PDFs/physics/StaircasePdf.h
+++ include/goofit/PDFs/physics/StaircasePdf.h
@@ -1,6 +1,3 @@
-#ifndef STAIRCASE_THRUST_FUNCTOR_HH
-#define STAIRCASE_THRUST_FUNCTOR_HH
-
 #include <goofit/PDFs/GooPdf.h> 
 #include <goofit/PdfBase.h>
 #include <goofit/Variable.h>
@@ -11,9 +8,7 @@ namespace GooFit {
 class StaircasePdf : public GooPdf {
  public:
   StaircasePdf (std::string n, Observable _x, std::vector<Variable> x0list);
-  //__host__ fptype integrate (fptype lo, fptype hi) const;
-  //__host__ virtual bool hasAnalyticIntegral () const {return true;}
+
  private:
 };
 }
-#endif

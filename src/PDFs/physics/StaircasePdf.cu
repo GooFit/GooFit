993a4b617ea2f58df1d3eee093f8dfb037e9bb95
martha <martha@hepgpu4.blackett.manchester.ac.uk>
2020-07-24 11:24:02 +0100

    Fix.

--- src/PDFs/physics/StaircasePdf.cu
+++ src/PDFs/physics/StaircasePdf.cu
@@ -8,56 +8,50 @@ namespace GooFit {
 
 __device__ fptype device_Staircase (fptype* evt, ParameterContainer &pc)
 {
-
-  int num_parameters  = pc.getNumParameters();
   int num_constants   = pc.getNumConstants();
+  int num_parameters  = pc.getNumParameters();
   int num_observables = pc.getNumObservables();
 
-  int id       = pc.getObservable(0);
-  fptype x     = RO_CACHE(evt[id]);
+  int id = pc.getObservable(0);
+  fptype x   = RO_CACHE(evt[id]);
 
+  unsigned int ret = num_parameters;
 
-  fptype ret;
-  fptype param;
-  for(unsigned int i = 2; i < num_parameters; i++)
+  // indices[1] should be the number of step points we have
+  for(unsigned int i = 0; i < num_parameters; i++)
   {
-    param = pc.getParameter(i);
-    if(x < param)
+    if(x < pc.getParameter(i))
     {
       ret = i;
       break;
     }
   }
-
+  
   pc.incrementIndex(1, num_parameters, num_constants, num_observables, 1);
+
 
   return ret;
 }
 
 __device__ device_function_ptr ptr_to_Staircase = device_Staircase;
-//device_function_ptr hptr_to_Staircase = device_Staircase;
+device_function_ptr hptr_to_Staircase = device_Staircase;
 
 __host__ StaircasePdf::StaircasePdf(std::string n, Observable _x, std::vector<Variable> x0list)
-  : GooPdf("StaircasePdf", n, _x, x0list) 
+  : GooPdf("StaircasePdf", n) 
 {
+  std::vector<unsigned int> pindices;
+  pindices.push_back(x0list.size());
+
   registerObservable(_x);
 
-  int numParameters = pc.getNumParameters();
-
-  for (i=0;i<numParameters;i++){
-    Variable newx0("newx0",0);
-    x0list.push_back(newx0);
-  }
-
-  for (Variable &x0 : x0list){
+  for(Variable &x0: x0list)
     registerParameter(x0);
-  }
 
   registerFunction("ptr_to_Staircase", ptr_to_Staircase);
 
   initialize();
 
-  //std::cout << "StaircasePdf::StaircasePdf(" << n << ", ...)" << std::endl;
 }
+
 
 } //namespace GooFit

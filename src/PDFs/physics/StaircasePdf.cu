492f329ef7fcbe2d8d2777aa08c753b67cb59839
martha <martha@hepgpu4.blackett.manchester.ac.uk>
2020-07-28 16:11:34 +0100

    Change to pointer, change incrementIndex.

--- src/PDFs/physics/StaircasePdf.cu
+++ src/PDFs/physics/StaircasePdf.cu
@@ -12,23 +12,24 @@ __device__ fptype device_Staircase (fptype* evt, ParameterContainer &pc)
   int num_parameters  = pc.getNumParameters();
   int num_observables = pc.getNumObservables();
 
+  int numComps = pc.getConstant(0);
+
   int id = pc.getObservable(0);
   fptype x   = RO_CACHE(evt[id]);
 
-  unsigned int ret = num_parameters;
+  fptype ret = 0;
 
-  // indices[1] should be the number of step points we have
+  pc.incrementIndex(1, 8, num_constants, num_observables, 1);
+
   for(unsigned int i = 0; i < num_parameters; i++)
   {
-    if(x < pc.getParameter(i))
+    fptype param = pc.getParameter(i);
+    if(x < param)
     {
       ret = i;
       break;
     }
   }
-  
-  pc.incrementIndex(1, num_parameters, num_constants, num_observables, 1);
-
 
   return ret;
 }
@@ -36,17 +37,17 @@ __device__ fptype device_Staircase (fptype* evt, ParameterContainer &pc)
 __device__ device_function_ptr ptr_to_Staircase = device_Staircase;
 device_function_ptr hptr_to_Staircase = device_Staircase;
 
-__host__ StaircasePdf::StaircasePdf(std::string n, Observable _x, std::vector<Variable> x0list)
+__host__ StaircasePdf::StaircasePdf(std::string n, Observable _x, std::vector<Variable*> x0list)
   : GooPdf("StaircasePdf", n) 
 {
-  std::vector<unsigned int> pindices;
-  pindices.push_back(x0list.size());
 
   registerObservable(_x);
 
-  for(Variable &x0: x0list)
-    registerParameter(x0);
+  for(Variable* &x0: x0list)
+    registerParameter(*x0);
 
+  registerConstant(x0list.size());
+  
   registerFunction("ptr_to_Staircase", ptr_to_Staircase);
 
   initialize();

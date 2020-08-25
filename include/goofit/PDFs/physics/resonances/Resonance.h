a71d26eeddf589018d638e1ca9c0acac55945fc5
Daniel Vieira <devieira@mbp-de-daniel.lan>
2020-08-25 17:13:57 -0400

    adding other files

--- include/goofit/PDFs/physics/resonances/Resonance.h
+++ include/goofit/PDFs/physics/resonances/Resonance.h
@@ -28,10 +28,18 @@ __device__ fpcomplex phsp_twoBody(fptype s, fptype m0, fptype m1);
 
 __device__ fpcomplex phsp_fourPi(fptype s);
 
+__device__ void getCofactor(fptype A[NCHANNELS][NCHANNELS], fptype temp[NCHANNELS][NCHANNELS], int p, int q, int n); 
+
+__device__ fptype determinant(fptype A[NCHANNELS][NCHANNELS], int n); 
+
+__device__ void adjoint(fptype A[NCHANNELS][NCHANNELS],fptype adj[NCHANNELS][NCHANNELS]); 
+
+__device__ bool inverse(fptype A[NCHANNELS][NCHANNELS], fptype inverse[NCHANNELS][NCHANNELS]);
+
 __device__ void
-getPropagator(const Eigen::Array<fptype, NCHANNELS, NCHANNELS> &kMatrix,
-              const Eigen::Matrix<fpcomplex, 5, 1> &phaseSpace,
-              Eigen::Matrix<fpcomplex, NCHANNELS, NCHANNELS> &F,
+getPropagator(const fptype kMatrix[NCHANNELS][NCHANNELS],
+              const  fpcomplex phaseSpace[NCHANNELS],
+              fpcomplex F[NCHANNELS][NCHANNELS],
               fptype adlerTerm);
 
 /**

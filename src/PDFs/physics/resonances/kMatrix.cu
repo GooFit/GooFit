c7bc20c62c599c4ae30abf858974fdbd9c024a88
Daniel Vieira <devieira@sleepy.geop.uc.edu>
2020-10-14 21:20:59 -0400

    Cleaning kMatrix.cu

--- src/PDFs/physics/resonances/kMatrix.cu
+++ src/PDFs/physics/resonances/kMatrix.cu
@@ -39,21 +39,18 @@ __device__ fpcomplex kMatrixRes(fptype m12, fptype m13, fptype m23, ParameterCon
 
     for(int i = 0; i < NCHANNELS; i++) {
         fscat[i] = pc.getParameter(idx++);
-        // printf("fscat(%i) = %f\n", i, fscat[i]);
     }
 
     // in the next two sets of parameters the index is used two times in the same line, therefore it must be incremented
     // two times afterwards
     for(int i = 0; i < NCHANNELS; i++) {
         beta[i] = fpcomplex(pc.getParameter(idx), pc.getParameter(idx + 1));
-        // printf("beta[%i] = (%f,%f)\n", i, beta[i].real(), beta[i].imag());
         idx++;
         idx++;
     }
 
     for(int i = 0; i < NCHANNELS; i++) {
         f_prod[i] = fpcomplex(pc.getParameter(idx), pc.getParameter(idx + 1));
-        // printf("f_prod[%i] = (%f,%f)\n", i, f_prod[i].real(), f_prod[i].imag());
         idx++;
         idx++;
     }
@@ -61,20 +58,14 @@ __device__ fpcomplex kMatrixRes(fptype m12, fptype m13, fptype m23, ParameterCon
     for(int i = 0; i < NPOLES; i++) {
         for(int j = 0; j < NPOLES; j++) {
             couplings[i][j] = pc.getParameter(idx++);
-            // printf("couplings(%i,%i) = %f\n", i, j, couplings[i][j]);
         }
         pmasses[i] = pc.getParameter(idx++);
-        // printf("pmasses(%i) = %f\n", i, pmasses[i]);
     }
 
     fptype s = (PAIR_12 == Mpair ? m12 : (PAIR_13 == Mpair ? m13 : m23));
-    // fptype s = 1.53351;
 
     // constructKMatrix
-
     fptype kMatrix[NCHANNELS][NCHANNELS];
-
-    // TODO: Make sure the order (k,i,j) is correct
 
     for(int i = 0; i < 5; i++) {
         for(int j = 0; j < 5; j++) {
@@ -83,20 +74,10 @@ __device__ fpcomplex kMatrixRes(fptype m12, fptype m13, fptype m23, ParameterCon
                 kMatrix[i][j] += couplings[k][i] * couplings[k][j] / (POW2(pmasses[k]) - s);
             if(i == 0 || j == 0) // Scattering term
                 kMatrix[i][j] += fscat[i + j] * (1 - s0_scatt) / (s - s0_scatt);
-            // printf("kMatrix(%i,%i) = %f\n",
-            //       i,
-            //      j,
-            //     kMatrix[i][j]
-            //  );
         }
     }
 
     fptype adlerTerm = (1. - sA0) * (s - sA * mPiPlus * mPiPlus / 2) / (s - sA0);
-    // printf("adlerTerm = %f, sA0 = %f, sA = %f, sA * mPiPlus * mPiPlus / 2 = %f \n",
-    //       adlerTerm,
-    //       sA0,
-    //       sA,
-    //       sA * mPiPlus * mPiPlus / 2);
 
     fpcomplex phaseSpace[NCHANNELS];
     phaseSpace[0] = phsp_twoBody(s, mPiPlus, mPiPlus);
@@ -105,37 +86,11 @@ __device__ fpcomplex kMatrixRes(fptype m12, fptype m13, fptype m23, ParameterCon
     phaseSpace[3] = phsp_twoBody(s, mEta, mEta);
     phaseSpace[4] = phsp_twoBody(s, mEta, mEtap);
 
-    fpcomplex tMatrix[NCHANNELS][NCHANNELS];
-    fpcomplex I[NCHANNELS][NCHANNELS];
-    // printf ("tMatrix << ");
-    for(unsigned int i = 0; i < NCHANNELS; ++i) {
-        for(unsigned int j = 0; j < NCHANNELS; ++j) {
-            tMatrix[i][j] = (i == j ? 1. : 0.) - fpcomplex(0, adlerTerm) * kMatrix[i][j] * phaseSpace[j];
-            // printf(
-            //       "(%f,%f), ",
-            //      tMatrix[i][j].real(),  tMatrix[i][j].imag());
-        }
-        // printf("\n");
-    }
     fpcomplex F[NCHANNELS][NCHANNELS];
     getPropagator(kMatrix, phaseSpace, F, adlerTerm);
 
-    for(int i = 0; i < 5; i++) {
-        for(int j = 0; j < 5; j++) {
-            I[i][j] = fpcomplex(0, 0);
-            for(int k = 0; k < 5; k++) {
-                I[i][j] = I[i][j] + F[i][k] * tMatrix[k][j];
-            }
-            // printf(
-            //       "I(%i,%i) = (%f,%f)\n",
-            //        i, j, I[i][j].real(), I[i][j].imag());
-            // printf(
-            //        "F(%i,%i) = (%f,%f)\n",
-            //        i, j, F[i][j].real(), F[i][j].imag());
-        }
-    }
 
-    // TODO: calculate out
+    // calculates output
     pc.incrementIndex(1, idx, 1, 0, 1);
 
     fpcomplex ret(0, 0), pole(0, 0), prod(0, 0);
@@ -145,19 +100,12 @@ __device__ fpcomplex kMatrixRes(fptype m12, fptype m13, fptype m23, ParameterCon
         for(int i = 0; i < NCHANNELS; i++) {
             fptype coupling = couplings[pterm][i];
             M += F[0][i] * coupling;
-            // printf("F[0][%i] = (%f,%f), coupling[%i][%i] = %f\n", i, F[0][i].real(), F[0][i].imag(), i, pterm,
-            // couplings[pterm][i]);
         }
-        // printf("M = (%f,%f), 1/(M^2-s) = %f\n", M.real(), M.imag(), 1/(POW2(pmasses[pterm]) - s));
         pole = M / (POW2(pmasses[pterm]) - s);
         ret  = ret + beta[pterm] * pole;
 
-        // printf("F[0][%i] = (%f,%f),  (1 - s0_prod) / (s - s0_prod) = %f\n", pterm, F[0][pterm].real(),
-        // F[0][pterm].imag(),  (1 - s0_prod) / (s - s0_prod));
         prod = F[0][pterm] * (1 - s0_prod) / (s - s0_prod);
         ret  = ret + f_prod[pterm] * prod;
-        // printf("pole = (%f,%f), prod = (%f,%f), return = (%f,%f) \n", pole.real(), pole.imag(), prod.real(),
-        // prod.imag(), ret.real(), ret.imag() );
     }
 
     return ret;

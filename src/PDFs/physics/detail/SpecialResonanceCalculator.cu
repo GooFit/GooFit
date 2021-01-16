073008d71d90dd39bbcac5f5041ceeb67390acc4
Daniel Vieira <danielsibemol@gmail.com>
2021-01-15 22:36:22 -0500

    fixing bug in Resonance.cu and SpecialResonanceCalculator.cu

--- src/PDFs/physics/detail/SpecialResonanceCalculator.cu
+++ src/PDFs/physics/detail/SpecialResonanceCalculator.cu
@@ -29,10 +29,10 @@ __device__ fpcomplex SpecialResonanceCalculator::operator()(thrust::tuple<int, f
     if(!inDalitz(m12, m13, c_motherMass, c_daug1Mass, c_daug2Mass, c_daug3Mass))
         return ret;
 
-    // This was being calculated with invariant masses instead of squared invariante masses and without taking the sqrt.
+    // m12, m23 and m13 stand for the squared invariant masses.
     // Now fixed.
-    fptype m23 = sqrt(c_motherMass * c_motherMass + c_daug1Mass * c_daug1Mass + c_daug2Mass * c_daug2Mass
-                      + c_daug3Mass * c_daug3Mass - m12 * m12 - m13 * m13);
+    fptype m23 = c_motherMass * c_motherMass + c_daug1Mass * c_daug1Mass + c_daug2Mass * c_daug2Mass
+                 + c_daug3Mass * c_daug3Mass - m12 - m13;
 
     while(pc.funcIdx < resonance_i)
         pc.incrementIndex();

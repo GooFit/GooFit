50cb24a7be6207bda346ca4b64cb4647e3fb06e5
Henry Schreiner <hschrein@cern.ch>
2021-09-29 07:07:03 -0400

    chore: use pip clang format (#276)
    * chore: use pip clang format
    
    * ci: job not needed
    
    * docs: docker not needed anymore
    
    * style: pre-commit fixes
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- include/goofit/Error.h
+++ include/goofit/Error.h
@@ -9,7 +9,7 @@ namespace GooFit {
 /// Thrown when a general error is encountered
 struct GeneralError : public CLI::Error {
     template <typename... Args>
-    GeneralError(std::string name, Args &&... args)
+    GeneralError(std::string name, Args &&...args)
         : Error("GeneralError", fmt::format(name, std::forward<Args>(args)...), 2) {}
 };
 

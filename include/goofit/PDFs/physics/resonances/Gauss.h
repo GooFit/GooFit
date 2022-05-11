e81cf9a3e77d0d06ea7b981dfa4612fbfdc2ae3c
juanbsleite <juanbsleite@live.com>
2022-05-11 10:56:22 -0300

    fix pipipi0 example

--- include/goofit/PDFs/physics/resonances/Gauss.h
+++ include/goofit/PDFs/physics/resonances/Gauss.h
@@ -9,7 +9,7 @@ namespace Resonances {
 /// Gaussian constructor
 class Gauss : public ResonancePdf {
   public:
-    Gauss(std::string name, Variable ar, Variable ai, Variable mean, Variable sigma, unsigned int cyc,bool symmDP);
+    Gauss(std::string name, Variable ar, Variable ai, Variable mean, Variable sigma, unsigned int cyc,bool symmDP=false);
     ~Gauss() override = default;
 };
 

f33df3261849730b1dac5b4ea6272b0935f76501
FlorianReiss <freiss@cern.ch>
2022-05-18 23:31:18 +0200

    feat: cherry-pick FitManager improvements (#313)
    * Add MnScan.
    
    * Add Minos error calculation.
    
    * cherry-pick Add Minos error calculation
    
    * Initial commit.
    
    * cherry-pick Add MnMinos
    
    * cherry-pick Adding minons
    
    * Add GetGooFitParams.
    
    * style: pre-commit fixes
    
    Co-authored-by: martha <martha@hepgpu4.blackett.manchester.ac.uk>
    Co-authored-by: martha <martha@hepgpu7.blackett.manchester.ac.uk>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- python/goofit/FitManager.cpp
+++ python/goofit/FitManager.cpp
@@ -3,18 +3,27 @@
 #include <pybind11/iostream.h>
 #include <pybind11/stl.h>
 
+#include <Minuit2/MnUserParameters.h>
+#include <Minuit2/FCNBase.h>
+
 #include <goofit/PdfBase.h>
 #include <goofit/PyProps.h>
 #include <goofit/fitting/FitManagerMinuit2.h>
+#include <goofit/fitting/Params.h>
 
 #include <iostream>
 
 using namespace GooFit;
 
 void init_FitManager(py::module &m) {
-    py::class_<FCN>(m, "FCN");
+    py::class_<FCN, Minuit2::FCNBase>(m, "FCN").def(py::init<Params &>(), "params"_a);
 
-    py::class_<Params>(m, "Params")
+    py::class_<Params, Minuit2::MnUserParameters>(m, "Params")
+        .def(py::init<PdfBase &>(), "pdf"_a)
+        .def("SetGooFitParams", &Params::SetGooFitParams, "input"_a)
+        .def("size", &Params::size)
+        .def("make_minuit_vector", &Params::make_minuit_vector)
+
         // clang-format off
         ADD_PROP_WO(record, set_record, Params)
         ADD_PROP_RO(recorded, get_recorded, Params)
@@ -24,7 +33,10 @@ void init_FitManager(py::module &m) {
     py::class_<FitManagerMinuit2>(m, "FitManager")
         .def(py::init<PdfBase *>())
         .def("fit", &FitManagerMinuit2::fit, py::call_guard<py::scoped_ostream_redirect>())
+        .def("getMinosErrors", &FitManagerMinuit2::getMinosErrors)
+        .def("getMnScan", &FitManagerMinuit2::getMnScan)
         .def("__int__", &FitManagerMinuit2::operator int)
+        .def("setMinos", &FitManagerMinuit2::setMinos)
         .def("__bool__", &FitManagerMinuit2::operator bool)    // Py3
         .def("__nonzero__", &FitManagerMinuit2::operator bool) // Py2
 

e9e2ee0747ec2268815f3101e4e154c649bfb4a9
jcob95 <john.leslie.cobbledick@cern.ch>
2022-05-25 12:41:42 +0100

    jcob changes to Amp4body_TD

--- k3pi_goofit_scripts/6D_normalisation_studies.py
+++ k3pi_goofit_scripts/6D_normalisation_studies.py
@@ -3,20 +3,56 @@ import numpy as np
 import pandas as pd
 import subprocess as sb
 import os
+import argparse
+from joblib import Parallel, delayed
 
 executable_name = "WS_model_fit"
 build_dir = "../build-cuda/k3pi_goofit_scripts/"
+gen_executable_name = "WS_model_gen"
+numDisjointSamples = 300
+x = "0.0045" 
+y = "0.0062"
+nGenEvts = 70000 # will need to be changed for the full Run 2 WS sample
 
-def fit_results_only_xy():
-    seeds = [i for i in range(100)]
+parser = argparse.ArgumentParser()
+parser.parse_args()
+
+def gen_disjoint_samples(gpu_id = 0,seeds=range(int(numDisjointSamples))):
+    #generate samples with x = 0.0045 and y = 0.0062 but with different seeds to generate different events for each toy
+    #We can then fit each toy (using the same normalisation, by default) to obtain a distribution of fitted x and y values over the ensemble of toys
+    for i in seeds:
+        batchNum = 1000000
+        nGenEvts = 70000 # will need to be changed for the full Run 2 WS sample
+        seed = i
+        output_file = build_dir+f"ws_generated_mc_samples/signal_only_{nGenEvts}_sample_num_{i}_x_{x}_y_{y}.txt"
+        command_arr = [build_dir+ gen_executable_name, str(batchNum), str(nGenEvts), str(seed), str(output_file), x, y, "--gpu-dev", str(gpu_id)]
+        output = None
+        try:
+            output = sb.check_output(command_arr)
+        except sb.CalledProcessError as e:
+            print(f"failed to execute command with seed: {seed}")
+            print(str(e.cmd))
+        print(output)
+
+def fit_disjoint_samples():
+    pass
+
+def fit_results_only_xy(gpu_id = 0,seeds=range(int(numDisjointSamples)),special_integral=True):
+    #seeds = [i for i in range(100)]
     for seed in seeds:
         sampleNum = "70000"
         x_start = "0.0045"
         y_start = "0.0062"
-        input_file = "../build-cuda/k3pi_goofit_scripts/ws_generated_mc_samples/signal_only_70k_sample_x_0.0045_y_0.0062.txt"
-        output_file = f"../build-cuda/k3pi_goofit_scripts/ws_signal_only_fit_results/signal_only_70k_sample_x_0.0045_y_0.0062_fit_results_seed_{seed}.txt"
-
-        command_arr = [build_dir + executable_name,str(sampleNum),str(x_start),str(y_start),str(input_file),str(output_file),"--specInt","--genOff",str(seed)]
+        
+        #input_file = "../build-cuda/k3pi_goofit_scripts/ws_generated_mc_samples/signal_only_70k_sample_x_0.0045_y_0.0062.txt"
+        input_file = build_dir+f"ws_generated_mc_samples/signal_only_{nGenEvts}_sample_num_{seed}_x_{x}_y_{y}.txt"
+        output_file = f"../build-cuda/k3pi_goofit_scripts/ws_signal_only_fit_results/signal_only_70k_sample_x_0.0045_y_0.0062_fit_results_sample_num_{seed}_same_norm_seed.txt"
+        #uncomment below if you want to change the normalisation seed when fitting
+        #command_arr = [build_dir + executable_name,str(sampleNum),str(x_start),str(y_start),str(input_file),str(output_file),"--specInt","--genOff",str(seed), "--gpu-dev", str(gpu_id)]
+        command_arr = [build_dir + executable_name,str(sampleNum),str(x_start),str(y_start),str(input_file),str(output_file),"--specInt", "--gpu-dev", str(gpu_id)]
+        if not special_integral:
+            output_file = f"../build-cuda/k3pi_goofit_scripts/ws_signal_only_fit_results/signal_only_70k_sample_x_0.0045_y_0.0062_fit_results_sample_num_{seed}_same_norm_seed_default_integral.txt"
+            command_arr = [build_dir + executable_name,str(sampleNum),str(x_start),str(y_start),str(input_file),str(output_file), "--gpu-dev", str(gpu_id)]
         print("executing command")
         output = None
         try:
@@ -26,16 +62,20 @@ def fit_results_only_xy():
             print(str(e.cmd))
         print(output)
         
-def fit_results_xy_floated_DCS():
-    seeds = [i for i in range(100)]
+def fit_results_xy_floated_DCS(gpu_id = 0,seeds=range(int(numDisjointSamples)),special_integral=True):
+    #seeds = [i for i in range(100)]
     for seed in seeds:
         sampleNum = "70000"
         x_start = "0.0045"
         y_start = "0.0062"
-        input_file = "../build-cuda/k3pi_goofit_scripts/ws_generated_mc_samples/signal_only_70k_sample_x_0.0045_y_0.0062.txt"
-        output_file = f"../build-cuda/k3pi_goofit_scripts/ws_signal_only_fit_results/signal_only_70k_sample_x_0.0045_y_0.0062_fit_results_seed_{seed}_floated_DCS.txt"
+        input_file = build_dir+f"ws_generated_mc_samples/signal_only_{nGenEvts}_sample_num_{seed}_x_{x}_y_{y}.txt"
+        output_file = f"../build-cuda/k3pi_goofit_scripts/ws_signal_only_fit_results/signal_only_70k_sample_x_0.0045_y_0.0062_fit_results_sample_num_{seed}_same_norm_seed_floated_dcs.txt"
 
-        command_arr = [build_dir + executable_name,str(sampleNum),str(x_start),str(y_start),str(input_file),str(output_file),"--specInt","--genOff",str(seed),"--floatDCS"]
+        #command_arr = [build_dir + executable_name,str(sampleNum),str(x_start),str(y_start),str(input_file),str(output_file),"--specInt","--genOff",str(seed),"--floatDCS", "--gpu-dev", str(gpu_id)]
+        command_arr = [build_dir + executable_name,str(sampleNum),str(x_start),str(y_start),str(input_file),str(output_file),"--specInt","--floatDCS" , "--gpu-dev", str(gpu_id)]
+        if not special_integral:
+            output_file = f"../build-cuda/k3pi_goofit_scripts/ws_signal_only_fit_results/signal_only_70k_sample_x_0.0045_y_0.0062_fit_results_sample_num_{seed}_same_norm_seed_floated_dcs_default_integral.txt"
+            command_arr = [build_dir + executable_name,str(sampleNum),str(x_start),str(y_start),str(input_file),str(output_file),"--floatDCS" , "--gpu-dev", str(gpu_id)]
         print("executing command")
         output = None
         try:
@@ -44,5 +84,12 @@ def fit_results_xy_floated_DCS():
             print(f"failed to execute command with seed: {seed}")
             print(str(e.cmd))
         print(output)
+#Parallel(n_jobs=2)(delayed(gen_disjoint_samples)(id,seeds) for (id,seeds) in [(0,range(int(numDisjointSamples/2))),(1,range(int(numDisjointSamples/2),numDisjointSamples))])
+#fit toys using 6D numerical normalisation integral
+#Parallel(n_jobs=2)(delayed(fit_results_only_xy)(id,seeds) for (id,seeds) in [(0,range(int(numDisjointSamples/2))),(1,range(int(numDisjointSamples/2),numDisjointSamples))])
+#Parallel(n_jobs=2)(delayed(fit_results_xy_floated_DCS)(id,seeds) for (id,seeds) in [(0,range(int(numDisjointSamples/2))),(1,range(int(numDisjointSamples/2),numDisjointSamples))])
+#fit toys using the default normalisation integral
+Parallel(n_jobs=2)(delayed(fit_results_only_xy)(id,seeds,special_int) for (id,seeds,special_int) in [(0,range(int(numDisjointSamples/2)),False),(1,range(int(numDisjointSamples/2),numDisjointSamples),False)])
+Parallel(n_jobs=2)(delayed(fit_results_xy_floated_DCS)(id,seeds,special_int) for (id,seeds,special_int) in [(0,range(int(numDisjointSamples/2)),False),(1,range(int(numDisjointSamples/2),numDisjointSamples),False)])
 #fit_results_only_xy()
-fit_results_xy_floated_DCS()
+#fit_results_xy_floated_DCS()
--- k3pi_goofit_scripts/plot_normalisation_studies.py
+++ k3pi_goofit_scripts/plot_normalisation_studies.py
@@ -28,9 +28,72 @@ def get_xy_values():
                 y_err.append(file_values[4])
     return (x_values,x_err,y_values,y_err)
 
+def get_xy_dcs_values():
+    files = glob.glob("../build-cuda/k3pi_goofit_scripts/ws_signal_only_fit_results/signal_only_70k_sample_x_0.0045_y_0.0062_fit_results_seed_*.txt")
+    x_values = []
+    x_err = []
+    y_values = []
+    y_err = []
+    fit_files = []
+    for f in files:
+        if "floated_DCS" in f:
+            fit_files.append(f)
+            file_values = np.loadtxt(f,delimiter=' ',dtype=str)
+            if int(file_values[5]) == 1 and int(file_values[6]) == 3:
+                x_values.append(float(file_values[1]))
+                x_err.append(float(file_values[2]))
+                y_values.append(float(file_values[3]))
+                y_err.append(float(file_values[4]))
+    return (x_values,x_err,y_values,y_err)
+
+def get_xy_ensemble_values():
+    files = glob.glob("../build-cuda/k3pi_goofit_scripts/ws_signal_only_fit_results/signal_only_70k_sample_x_0.0045_y_0.0062_fit_results_sample_num_*_same_norm_seed.txt")
+    files_default = glob.glob("../build-cuda/k3pi_goofit_scripts/ws_signal_only_fit_results/signal_only_70k_sample_x_0.0045_y_0.0062_fit_results_sample_num_*_same_norm_seed_default_integral.txt")
+    x_values = []
+    y_values = []
+    x_values_default = []
+    y_values_default = []
+    fit_files = []
+    for f,f_default in zip(files,files_default):
+        if "floated_DCS" not in f:
+            fit_files.append(f)
+            file_values = np.loadtxt(f,delimiter=' ',dtype=str)
+            if int(file_values[5]) == 1 and int(file_values[6]) == 3:
+                x_values.append(float(file_values[1]))
+                y_values.append(float(file_values[3]))
+            fit_values = np.loadtxt(f_default,delimiter=' ',dtype=str)
+            if int(file_values[5]) == 1 and int(file_values[6]) == 3:
+                x_values_default.append(float(file_values[1]))
+                y_values_default.append(float(file_values[3]))
+    return (x_values,x_values_default,y_values,y_values_default)
+
+def get_xy_ensemble_dcs_values():
+    files = glob.glob("../build-cuda/k3pi_goofit_scripts/ws_signal_only_fit_results/signal_only_70k_sample_x_0.0045_y_0.0062_fit_results_sample_num_*_same_norm_seed_floated_dcs.txt")
+    files_default = glob.glob("../build-cuda/k3pi_goofit_scripts/ws_signal_only_fit_results/signal_only_70k_sample_x_0.0045_y_0.0062_fit_results_sample_num_*_same_norm_seed_floated_dcs_default_integral.txt")
+    x_values = []
+    y_values = []
+    x_values_default = []
+    y_values_default = []
+    fit_files = []
+    for f,f_default in zip(files,files_default):
+        with open(f) as f_open:
+            file_values = [x.split() for x in f_open.readlines()]
+            file_values = [item for sublist in file_values for item in sublist]
+            if int(file_values[5]) == 1 and int(file_values[6]) == 3:
+                x_values.append(float(file_values[1]))
+                y_values.append(float(file_values[3]))
+            f_open.close()
+        with open(f_default) as f_open:
+            file_values = [x.split() for x in f_open.readlines()]
+            file_values = [item for sublist in file_values for item in sublist]
+            if int(file_values[5]) == 1 and int(file_values[6]) == 3:
+                x_values_default.append(float(file_values[1]))
+                y_values_default.append(float(file_values[3]))
+            f_open.close()
+    return (x_values,x_values_default,y_values,y_values_default)
 
 
-def fit_array(arr,varname):
+def fit_array(arr,varname,dcs=False,ensemble=False,default_int=False):
     low_lim,high_lim =( 0.95*min(arr),1.05*max(arr))
     var = ROOT.RooRealVar(varname,varname,0.9*min(arr),1.1*max(arr))
     mean = ROOT.RooRealVar(varname + " mean",varname + " Gaussian mean",(low_lim + high_lim)/2, low_lim,high_lim)
@@ -63,37 +126,41 @@ def fit_array(arr,varname):
         xline.SetLineWidth(5)
         xline.Draw()
     pt.Draw()
-    c.SaveAs(varname+"_fit_xy_floated.png")
+    canvas_name = varname +"_fit_xy"
+    if dcs:
+        canvas_name += "_dcs_floated"
+    if ensemble:
+        canvas_name += "_ensemble"
+    if default_int:
+        canvas_name += "_default_integral"
+    canvas_name += ".png"
+    c.SaveAs(canvas_name)
+    print(f"Fit results for {varname} with dcs floated:{dcs}")
+    print(f"Mean:{mean.getValV()}")
+    print(f"Sigma:{sigma.getValV()}")
 
 
+#results for fitting unique toys with the same set of normalisation events
+x_values,x_values_default,y_values,y_values_default = get_xy_ensemble_values()
 
-x_values, x_err, y_values, y_err = get_xy_values()
-fit_array(x_values,"fitted x")
-#fit_array(x_err,"fitted x error")
-fit_array(y_values,"fitted y")
-#fit_array(y_err,"fitted y error")
+x_values_dcs,x_values_default_dcs,y_values_dcs,y_values_default_dcs =  get_xy_ensemble_dcs_values()
+fit_array(x_values_dcs,"fitted x",dcs=True,ensemble=True)
+fit_array(y_values_dcs,"fitted y",dcs=True,ensemble=True)
+fit_array(x_values_default_dcs,"fitted x",dcs=True,ensemble=True,default_int=True)
+fit_array(y_values_default_dcs,"fitted y",dcs=True,ensemble=True,default_int=True)
+
+fit_array(x_values,"fitted x",dcs=False,ensemble=True)
+fit_array(y_values,"fitted y",dcs=False,ensemble=True)
+fit_array(x_values_default,"fitted x",dcs=False,ensemble=True,default_int=True)
+fit_array(y_values_default,"fitted y",dcs=False,ensemble=True,default_int=True)
 
 """
-x = ROOT.RooRealVar("x","x",0.0044,0.005)
-mean = ROOT.RooRealVar("mean","x Gaussian mean",0.0045, 0.004,0.005)
-sigma = ROOT.RooRealVar("sigma","x Gaussian width",0.001,0.,1.)
-gauss = ROOT.RooGaussian("gauss","gaussian PDF",x,mean,sigma)
-x_dataset = ROOT.RooDataSet("x_data","x_data",ROOT.RooArgSet(x))
-for x_val in x_values:
-    x.setVal(x_val)
-    x_dataset.add(ROOT.RooArgSet(x))
-gauss.fitTo(x_dataset)
-
-c = ROOT.TCanvas()
-xframe = x.frame()
-x_dataset.plotOn(xframe)
-gauss.plotOn(xframe)
-xframe.GetXaxis().SetTitle("Fitted x value")
-xframe.SetTitle("")
-xframe.Draw("E0")
-xline = ROOT.TLine(0.0045,0,0.0045,15)
-xline.SetLineColor(ROOT.kRed)
-xline.SetLineWidth(5)
-xline.Draw()
-c.SaveAs("fitted_x_values_xy_floated.png")
+#results for fitting the same toy multiple times. Used for estimating error on 6D integration
+x_values, x_err, y_values, y_err = get_xy_values()
+x_values_dcs,x_err_dcs,y_values_dcs,y_err_dcs = get_xy_dcs_values()
+print(x_values_dcs)
+fit_array(x_values_dcs,"fitted x",dcs=True)
+fit_array(y_values_dcs,"fitted y",dcs=True)
+fit_array(x_values,"fitted x",dcs=False)
+fit_array(y_values,"fitted y",dcs=False)
 """

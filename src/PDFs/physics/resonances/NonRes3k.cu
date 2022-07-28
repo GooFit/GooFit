6b50d4615df8c662bf624297d58daed5b9a06f7a
juanbsleite <juanbsleite@live.com>
2022-07-28 16:17:14 -0300

    NonRes3K fix

--- src/PDFs/physics/resonances/NonRes3k.cu
+++ src/PDFs/physics/resonances/NonRes3k.cu
@@ -8,11 +8,15 @@ namespace GooFit {
 __device__ auto nonres3k(fptype m12, fptype m13, fptype m23, ParameterContainer &pc) -> fpcomplex {
     fptype alpha  = pc.getParameter(0);
 
-    fptype exp12 = exp(-alpha*m12);
-    fptype exp13 = exp(-alpha*m13);
-
     pc.incrementIndex(1, 1, 0, 0, 1);
-    return (exp12+exp13);
+
+    if(sqrt(m12)>2.0 || sqrt(m13)>2.0){
+        fptype exp12 = exp(-alpha*m12);
+        fptype exp13 = exp(-alpha*m13);
+        return fpcomplex(exp12+exp13,0.0);
+    }else{
+        return fpcomplex(0.0,0.0);
+    }
 }
 
 __device__ resonance_function_ptr ptr_to_NONRES3k = nonres3k;

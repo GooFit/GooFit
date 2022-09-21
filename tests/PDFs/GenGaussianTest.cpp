d60a8707c91d617612f315b0af2cd81dc812a60a
Thomas Boettcher <boettcts@ucmail.uc.edu>
2022-09-21 10:35:39 -0400

    Fix gauss test

--- tests/PDFs/GenGaussianTest.cpp
+++ tests/PDFs/GenGaussianTest.cpp
@@ -36,7 +36,7 @@ TEST_CASE_METHOD(PdfTest1D, "Gaussian", "[fit][gaussian]") {
     int n = 10000;
     double merr = mean.getValue() / sqrt(n);
     double serr = sigma.getValue() / sqrt(n);
-    CHECK(mean.getError() < 3.*err);
+    CHECK(mean.getError() < 3.*merr);
     CHECK(mean.getValue() == Approx(mean_ans).epsilon(3./sqrt(n)));
 
     CHECK(sigma.getError() < 3.*serr);
